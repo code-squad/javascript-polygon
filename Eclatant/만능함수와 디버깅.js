a9dc2036ed43c913846a88493ad2ae2dce1eb4f0
Jeewhan <distinct19@gmail.com>
2017-10-17 17:59:56 +0900

    edit func
    * 함수에서 필요로 하는 argument를 array가 아닌 변수로 나누어 전달하도록 수정하였습니다.
    * 인수 전달방식이 바뀌면서 숫자인지 검사하는 함수의 인수구성도 변경하였습니다.

--- Eclatant/만능함수와 디버깅.js
+++ Eclatant/만능함수와 디버깅.js
@@ -10,9 +10,9 @@
 // getArea('circle', 1, 100);
 // > 반지름이 1부터 100까지 1씩 증가하면서, 100개의 원의 넓이의 결과값을 출력.
 
-function checkArgNaN(arg, predictArgNumber) {
-  for (var i = 0; i < predictArgNumber; i += 1) {
-    if (typeof arg[i] !== "number") {
+function checkArgNaN(predictArgNumber) {
+  for (var i = 1; i <= predictArgNumber; i += 1) {
+    if (typeof arguments[i] !== "number") {
       console.log("숫자형 타입만 계산이 가능합니다!");
       return true;
     }
@@ -21,78 +21,78 @@ function checkArgNaN(arg, predictArgNumber) {
   return false;
 }
 
-function getCircle(numArr) {
-  if (numArr.length !== 1) {
+function getCircle(radius) {
+  if (arguments.length !== 1) {
     console.log("1개의 인자가 필요합니다.");
     return false;
   }
 
-  if (checkArgNaN(numArr, 1)) {
+  if (checkArgNaN(1, radius)) {
     return false;
   }
 
-  if (numArr[0] <= 0) {
+  if (radius <= 0) {
     console.log("반지름은 0보다 커야 합니다.");
     return false;
   }
 
-  var circleArea = Math.pow(numArr[0], 2) * Math.PI;
+  var circleArea = Math.pow(radius, 2) * Math.PI;
 
   return circleArea;
 }
 
-function getRect(numArr) {
-  if (numArr.length !== 2) {
+function getRect(base, height) {
+  if (arguments.length !== 2) {
     console.log("2개의 인자가 필요합니다.");
     return false;
   }
 
-  if (checkArgNaN(numArr, 2)) {
+  if (checkArgNaN(2, base, height)) {
     return false;
   }
 
-  if (numArr[0] <= 0) {
+  if (base <= 0) {
     console.log("밑변은 0보다 커야 합니다.");
     return false;
   }
 
-  if (numArr[1] <= 0) {
+  if (height <= 0) {
     console.log("높이는 0보다 커야 합니다.");
     return false;
   }
 
-  var rectArea = numArr[0] * numArr[1];
+  var rectArea = base * height;
 
   return rectArea;
 }
 
-function getTrapezoid(numArr) {
-  if (numArr.length !== 3) {
+function getTrapezoid(base, upperSide, height) {
+  if (arguments.length !== 3) {
     console.log("3개의 인자가 필요합니다.");
     return false;
   }
 
-  if (checkArgNaN(numArr, 3)) {
+  if (checkArgNaN(3, base, upperSide, height)) {
     console.log("숫자형 타입만 계산이 가능합니다.");
     return false;
   }
 
-  if (numArr[0] <= 0) {
+  if (base <= 0) {
     console.log("밑변은 0보다 커야 합니다.");
     return false;
   }
 
-  if (numArr[1] <= 0) {
+  if (upperSide <= 0) {
     console.log("윗변은 0보다 커야 합니다.");
     return false;
   }
 
-  if (numArr[2] <= 0) {
+  if (height <= 0) {
     console.log("높이는 0보다 커야 합니다.");
     return false;
   }
 
-  var trapezoidArea = numArr[0] * numArr[1] * numArr[2] / 2;
+  var trapezoidArea = base * upperSide * height / 2;
 
   return trapezoidArea;
 }
@@ -100,17 +100,15 @@ function getTrapezoid(numArr) {
 // Array.prototype 사용하기 : http://www.bsidesoft.com/?p=323
 // arguments는 완전한 배열이 아니므로 slice를 하려면 아래처럼 코드를 써야 한다
 function getArea(calculateCase) {
-  var arg = Array.prototype.slice.call(arguments, 1);
-
   switch (calculateCase) {
     case "circle":
-      console.log(getCircle(arg));
+      console.log(getCircle(arguments[1]));
       break;
     case "rect":
-      console.log(getRect(arg));
+      console.log(getRect(arguments[1], arguments[2]));
       break;
     case "trapezoid":
-      console.log(getTrapezoid(arg));
+      console.log(getTrapezoid(arguments[1], arguments[2], arguments[2]));
       break;
   }
 }
