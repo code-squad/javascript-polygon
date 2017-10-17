6c1e22bf1d05bd3898efdfa03b7d01ae082d74ff
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-17 16:16:39 +0900

    Repactor error check

--- area.js
+++ area.js
@@ -1,10 +1,14 @@
 "use strict";
 
+function requireArguments(num) {
+    return num + "개의 인자가 필요합니다";
+};
+
 function checkPositive(text) {
     return text + "은(는) 0보다 커야 합니다";
 };
 
-function checkValue(len, theArgs) {
+function checkValue(type, len, theArgs) {
     if (len === 0) {
         return "최소 한가지 값이 필요합니다";
     }
@@ -13,98 +17,97 @@ function checkValue(len, theArgs) {
             return "숫자형타입만 계산이 가능합니다.";
         }
     }
+    switch (type) {
+        case "circle":
+            if (len !== 1) return requireArguments(1);
+            if (theArgs[0] <= 0) return checkPositive("반지름");
+            break;
+        case "rect":
+            if (len !== 2) return requireArguments(2);
+            if (theArgs[0] <= 0) return checkPositive("밑변");
+            if (theArgs[1] <= 0) return checkPositive("높이");
+            break;
+        case "trapezoid":
+            if (len !== 3) return requireArguments(3);
+            if (theArgs[0] <= 0) return checkPositive("밑변");
+            if (theArgs[1] <= 0) return checkPositive("윗변");
+            if (theArgs[2] <= 0) return checkPositive("높이");
+            break;
+        default:
+            break;
+    }
     return false;
 };
 
 function calculateCircle(radius) {
-    var theArgs = [];
-    var key = 0;
-    for (var len = arguments.length; key < len; key++) {
-        theArgs[key] = arguments[key];
-    }
+    var theArgs = Array.prototype.slice.call(arguments);
 
-    var length = theArgs.length;
-    var requireArguments = "1개의 인자가 필요합니다";
-    var checkErr = checkValue(length, theArgs);
-    if (checkErr) return checkErr;
-    if (length !== 1) return requireArguments;
-    if (theArgs[0] <= 0) return checkPositive("반지름");
+    var checkErr = checkValue("circle", theArgs.length, theArgs);
+    if (checkErr) return console.log(checkErr);
+
+
     var circle = Math.PI * radius * radius;
 
     execution.push("circle");
-    return circle;
+    return console.log(circle);
 };
 
 function calculateRect(width, height) {
-    var theArgs = [];
-    var key = 0;
-    for (var len = arguments.length; key < len; key++) {
-        theArgs[key] = arguments[key];
-    }
+    var theArgs = Array.prototype.slice.call(arguments);
 
-    var length = theArgs.length;
-    var requireArguments = "2개의 인자가 필요합니다";
-    var checkErr = checkValue(length, theArgs);
-    if (checkErr) return checkErr;
-    if (length !== 2) return requireArguments;
-    if (theArgs[0] <= 0) return checkPositive("밑변");
-    if (theArgs[1] <= 0) return checkPositive("높이");
+    var checkErr = checkValue("rect", theArgs.length, theArgs);
+    if (checkErr) return console.log(checkErr);
+
+
     var rect = width * height;
 
-
     execution.push("rect");
-    return rect;
+    return console.log(rect);
 };
 
 function calculateTrapezoid(base, upper, height) {
-    var theArgs = [];
-    var key = 0;
-    for (var len = arguments.length; key < len; key++) {
-        theArgs[key] = arguments[key];
-    }
+    var theArgs = Array.prototype.slice.call(arguments);
 
-    var length = theArgs.length;
-    var requireArguments = "3개의 인자가 필요합니다";
-    var checkErr = checkValue(length, theArgs);
-    if (checkErr) return checkErr;
-    if (length !== 3) return requireArguments;
-    if (theArgs[0] <= 0) return checkPositive("밑변");
-    if (theArgs[1] <= 0) return checkPositive("윗변");
-    if (theArgs[2] <= 0) return checkPositive("높이");
+    var checkErr = checkValue("trapezoid", theArgs.length, theArgs);
+    if (checkErr) return console.log(checkErr);
+
+
     var trapezoid = (base + upper) / 2 * height;
 
-
     execution.push("trapezoid");
-    return trapezoid;
+    return console.log(trapezoid);
 };
 
-function getArea(name) {
-    switch (name) {
+function getArea(type) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    var length = args.length;
+    var checkErr = checkValue(type, length, args);
+    if (checkErr && !(type === "circle" && length === 2)) return console.log(checkErr);
+    switch (type) {
         case "circle":
-            var length = arguments.length <= 1 ? 0 : arguments.length - 1;
-            var radius = arguments.length <= 1 ? undefined : arguments[1];
+            var radius = args[0];
             if (length === 1) {
-                console.log(calculateCircle(radius));
+                calculateCircle(radius);
             } else if (length === 2) {
-                var count = arguments.length <= 2 ? undefined : arguments[2];
+                var count = args[1];
                 for (var i = radius; i <= count; i++) {
-                    console.log(calculateCircle(i));
+                    calculateCircle(i);
                 }
             }
             break;
         case "rect":
-            var width = arguments.length <= 1 ? undefined : arguments[1];
-            var height = arguments.length <= 2 ? undefined : arguments[2];
-            console.log(calculateRect(width, height));
+            var width = args[0];
+            var height = args[1];
+            calculateRect(width, height);
             break;
         case "trapezoid":
-            var base = arguments.length <= 1 ? undefined : arguments[1];
-            var upper = arguments.length <= 2 ? undefined : arguments[2];
-            var height = arguments.length <= 3 ? undefined : arguments[3];
-            console.log(calculateTrapezoid(base, upper, height));
+            var base = args[0];
+            var upper = args[1];
+            var height = args[2];
+            calculateTrapezoid(base, upper, height);
             break;
         default:
-            console.log("Sorry, we are out of " + name + ".");
+            console.log("Sorry, we are out of " + type + ".");
     }
 };
 
