1425821da7a758ade738de224c7338b83a766f67
Jeewhan <distinct19@gmail.com>
2017-10-17 16:55:16 +0900

    rename func
    * 함수명은 동사+명사 형태를 취할 것

--- Eclatant/다각형을 계산하는 함수.js
+++ Eclatant/다각형을 계산하는 함수.js
@@ -1,4 +1,4 @@
-function circle(radius) {
+function getCircle(radius) {
   if (arguments.length !== 1) {
     console.log("1개의 인자가 필요합니다.");
     return null;
@@ -9,7 +9,7 @@ function circle(radius) {
   return result;
 }
 
-function rectangle(base, height) {
+function getRectangle(base, height) {
   if (arguments.length !== 2) {
     console.log("2개의 인자가 필요합니다.");
 
@@ -21,7 +21,7 @@ function rectangle(base, height) {
   return result;
 }
 
-function trapezoid(base, upperSide, height) {
+function getTrapezoid(base, upperSide, height) {
   if (arguments.length !== 3) {
     console.log("3개의 인자가 필요합니다.");
 
