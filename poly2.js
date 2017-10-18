90f6561108b910bc3fafec3118747d7c2cc392d7
wfe-luigi <kdh3979@naver.com>
2017-10-19 01:41:50 +0900

    함수명 수정, 인자비교 수정

--- poly2.js
+++ poly2.js
@@ -2,13 +2,14 @@
 //3개의 함수를 정의,호출 해서 그 결과를 한줄씩 출력. 
 //인자가 부족할 시 'n개의 인자가 필요합니다.' 라는 메세지를 출력
 
-function needMoreArg(arg){
+//인자부족시 에러메세지 출력
+function printErrMsg(arg){
     console.log(arg+"개의 인자가 필요합니다.");
 }
 //원 넓이 구하기
 function getCircle(radius){
     //if(!radius) return console.log("인자가 부족합니다.");
-    if(!arguments.length) needMoreArg(1);
+    if(arguments.length<1) printErrMsg(1);
     else console.log(radius * radius * Math.PI);
 }
 getCircle();
@@ -17,7 +18,7 @@ getCircle(5);
 //사각형넓이 구하기
 function getRect(width,height){
     if(width && height) return console.log(width * height);
-    needMoreArg(2);
+    printErrMsg(2);
 }
 getRect(5);
 getRect(5,10);
@@ -25,8 +26,12 @@ getRect(5,10);
 //사다리꼴 넓이 구하기
 function getTrap(bottom,top,height){
     if(bottom && top && height) return console.log((bottom + top) * height / 2);
-    needMoreArg(3);
-    
+    printErrMsg(3);
 }
+(function(a,b,c) {
+    if(a&&b&&c) console.log("ok");
+    else console.log("oh..");
+    })(1,null,undefined);
+
 getTrap(5,10);
 getTrap(5,10,15);
