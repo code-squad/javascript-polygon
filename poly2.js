76a1c30e0870e4404f8d0c403e04d0524dd32139
wfe-luigi <kdh3979@naver.com>
2017-10-19 11:20:16 +0900

    매개변수 예외처리

--- poly2.js
+++ poly2.js
@@ -2,9 +2,10 @@
 //3개의 함수를 정의,호출 해서 그 결과를 한줄씩 출력. 
 //인자가 부족할 시 'n개의 인자가 필요합니다.' 라는 메세지를 출력
 
-//인자부족시 에러메세지 출력
+//인자부족시 에러메세지 출력, 인자값이 0 일때 에러메세지 출력
 function printErrMsg(arg){
-    console.log(arg+"개의 인자가 필요합니다.");
+    if(arg == 0) console.log("인자의 값은 0 이상을 입력해주세요")
+    else console.log(arg+"개의 인자가 필요합니다.");
 }
 //원 넓이 구하기
 function getCircle(radius){
@@ -25,13 +26,20 @@ getRect(5,10);
 
 //사다리꼴 넓이 구하기
 function getTrap(bottom,top,height){
-    if(bottom && top && height) return console.log((bottom + top) * height / 2);
-    printErrMsg(3);
+    if(arguments.length<3) return printErrMsg(3);
+    else if(bottom <= 0){ console.log("0보다 큰 bottom값을 입력해주세요"); }
+    else if(top <= 0) { console.log("0보다 큰 top값을 입력해주세요"); }
+    else if(height <= 0) { console.log("0보다 큰 height값을 입력해주세요"); }
+    else return console.log((bottom + top) * height / 2);
 }
+/*
 (function(a,b,c) {
-    if(a&&b&&c) console.log("ok");
+    if(a&b&c) console.log("ok");
     else console.log("oh..");
     })(1,null,undefined);
-
+*/
 getTrap(5,10);
 getTrap(5,10,15);
+getTrap(-5,5,10);
+getTrap(5,-2,10);
+getTrap(6,4,-5);
