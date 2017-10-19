8cb889ea92de8559fffa80f9520c9fb337d9b7e3
Jeewhan <distinct19@gmail.com>
2017-10-19 11:42:01 +0900

    Refacforing 배열을 이용한 로깅
    * 변수 초기화는 상단에서 한다
    * 오류 체크만 하는 하는 함수를 만들어 독립시킨다

--- Eclatant/배열을 이용한 로깅.js
+++ Eclatant/배열을 이용한 로깅.js
@@ -15,9 +15,20 @@
 // getReport() //계산수행순서를 저장했다가 자유롭게 그 내역을 출력해보자. 배열을 사용할 수 있다.
 // > 계산수행순서 : circle, circle, circle, rect
 
-function checkArgTypeofNumber(arg, predictArgNumber) {
-  for (var i = 0; i < predictArgNumber; i += 1) {
-    if (typeof arg[i] !== "number") {
+function checkNegativeNumber(argArray) {
+  for (var i = 0; i < argArray.length; i += 1) {
+    if (argArray[i] <= 0) {
+      console.log("모든 인수는 양수여야 합니다.");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkArgTypeofNumber(argArray) {
+  for (var i = 0; i < argArray.length; i += 1) {
+    if (typeof argArray[i] !== "number") {
       console.log("숫자형 타입만 계산이 가능합니다!");
       return true;
     }
@@ -26,114 +37,85 @@ function checkArgTypeofNumber(arg, predictArgNumber) {
   return false;
 }
 
-function getCircle(numArr) {
-  if (numArr.length !== 1) {
-    console.log("1개의 인자가 필요합니다.");
-    return;
+function checkArgLength(argArray, correct) {
+  if (argArray.length !== correct) {
+    console.log(correct + "개의 인자가 필요합니다.");
+    return false;
   }
 
-  if (checkArgTypeofNumber(numArr, 1)) {
+  return true;
+}
+
+function getCircle(radius) {
+  var circleArea = Math.pow(radius, 2) * Math.PI;
+  var argArray = [radius];
+
+  if (
+    checkArgLength(argArray, 1) &&
+    checkArgTypeofNumber(argArray) &&
+    checkNegativeNumber(argArray)
+  ) {
     return;
   }
-
-  if (numArr[0] <= 0) {
-    console.log("반지름은 0보다 커야 합니다.");
-    return;
-  }
-
-  var result = Math.pow(numArr[0], 2) * Math.PI;
 
   calculateCount.circle += 1;
   calculateCount.commandOrder.push("circle");
 
-  var count = calculateCount.circle;
-  console.log("계산이 " + count + "번 일어났습니다.");
+  console.log("원 넓이 계산이 " + calculateCount.circle + "번 일어났습니다.");
 
-  return result;
+  return circleArea;
 }
 
-function getRect(numArr) {
-  if (numArr.length !== 2) {
-    console.log("2개의 인자가 필요합니다.");
+function getRect(base, height) {
+  var rectArea = base * height;
+  var argArray = [base, height];
+
+  if (
+    checkArgLength(argArray, 2) &&
+    checkArgTypeofNumber(argArray) &&
+    checkNegativeNumber(argArray)
+  ) {
     return;
   }
-
-  if (checkArgTypeofNumber(numArr, 2)) {
-    return;
-  }
-
-  if (numArr[0] <= 0) {
-    console.log("밑변은 0보다 커야 합니다.");
-    return;
-  }
-
-  if (numArr[1] <= 0) {
-    console.log("높이는 0보다 커야 합니다.");
-    return;
-  }
-
-  var result = numArr[0] * numArr[1];
 
   calculateCount.rect += 1;
   calculateCount.commandOrder.push("rect");
 
-  var count = calculateCount.rect;
-  console.log("계산이 " + count + "번 일어났습니다.");
+  console.log("사각형 넓이 계산이 " + calculateCount.rect + "번 일어났습니다.");
 
-  return result;
+  return rectArea;
 }
 
-function getTrapezoid(numArr) {
-  if (numArr.length !== 3) {
-    console.log("3개의 인자가 필요합니다.");
+function getTrapezoid(base, upperSide, height) {
+  var trapezoidArea = base * upperSide * height / 2;
+  var argArray = [base, upperSide, height];
+
+  if (
+    checkArgLength(argArray, 3) &&
+    checkArgTypeofNumber(argArray) &&
+    checkNegativeNumber(argArray)
+  ) {
     return;
   }
-
-  if (checkArgTypeofNumber(numArr, 3)) {
-    console.log("숫자형 타입만 계산이 가능합니다.");
-    return;
-  }
-
-  if (numArr[0] <= 0) {
-    console.log("밑변은 0보다 커야 합니다.");
-    return;
-  }
-
-  if (numArr[1] <= 0) {
-    console.log("윗변은 0보다 커야 합니다.");
-    return;
-  }
-
-  if (numArr[2] <= 0) {
-    console.log("높이는 0보다 커야 합니다.");
-    return;
-  }
-
-  var result = numArr[0] * numArr[1] * numArr[2] / 2;
 
   calculateCount.trapezoid += 1;
   calculateCount.commandOrder.push("trapezoid");
 
-  var count = calculateCount.trapezoid;
-  console.log("계산이 " + count + "번 일어났습니다.");
+  console.log("사다리꼴 넓이 계산이 " + calculateCount.trapezoid + "번 일어났습니다.");
 
-  return result;
+  return trapezoidArea;
 }
 
-// Array.prototype 사용하기 : http://www.bsidesoft.com/?p=323
-// arguments는 완전한 배열이 아니므로 slice를 하려면 아래처럼 코드를 써야 한다
 function getArea(calculateCase) {
-  var arg = Array.prototype.slice.call(arguments, 1);
-
   switch (calculateCase) {
     case "circle":
-      console.log(getCircle(arg));
+      console.log(getCircle(arguments[1]));
       break;
     case "rect":
-      console.log(getRect(arg));
+      console.log(getRect(arguments[1], arguments[2]));
       break;
     case "trapezoid":
-      console.log(getTrapezoid(arg));
+      console.log(getTrapezoid(arguments[1], arguments[2], arguments[3]));
       break;
   }
 }
@@ -148,3 +130,7 @@ var calculateCount = {
 function getReport() {
   console.log(calculateCount.commandOrder.join(", "));
 }
+
+getArea("circle", 10);
+getArea("rect", 10, 30);
+getArea("trapezoid", 10, 20, 30);
