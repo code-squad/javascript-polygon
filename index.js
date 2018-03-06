c8066374831d06f9818c09c2384365ab21f80388
chany <lyhy0310@gmail.com>
2018-03-06 16:38:08 +0900

    caculate function modify

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ let hasUndefinded = (arguments)=> {
     }
 }
 
+
 function calculateCircleSize(radius1,radius2) {
     var size = 0;
     if(radius2 === undefined){
-        size = radius1 * radius1 * Math.PI;
-        console.log("원의 넓이: " + size);
-        return size;
+        if(radius1 !== undefined){
+            if(typeof radius1 === "number"){
+                if(radius1 > 0){
+                    size = radius1 * radius1 * Math.PI;
+                    console.log("원의 넓이: " + size);
+                    return size;
+                }
+                else {
+                    console.log('반지름은 0보다 커야 합니다'); 
+                }
+            }
+            else {
+                console.log('숫자형타입만 계산이 가능합니다.')
+            }
+        }
+        else {
+            console.log('최소 한가지 값이 필요합니다');
+        }
+       
+       
     }
     if(arguments.length === 2){
         for(var i = radius1; i <= radius2; i++){
@@ -57,13 +75,6 @@ function getArea(shape, para1, para2, para3) {
             break;
     }
 }
-//요구사항
-// 1. 반지름이 10cm인 원 넓이를 출력한다.
-// 2. 밑변 10cm, 높이15cm인 사각형의 넓이를 출력한다.
-// 3. 밑변 10cm, 윗변 5cm, 높이가 12cm인 사다리꼴의 넓이를 출력한다.
-//다각형의 넓이를 함수를 호출해서 실행하도록 하자. 3개의 함수를 정의하고,
-// 3개의 함수를 호출해서 그 결과를 한줄씩 출력한다. 
-//사다리꼴의 넓이를 구하기 위해서는 3개의 인자를 받아야한다. 만약 2개의 인자만 받으면 '3개의 인자가 필요합니다' 라는 메시지를 노출하자.
 
 // 요구사항 1 
 console.log(getArea('circle',10));
@@ -73,4 +84,18 @@ console.log(getArea('rect', 10,15));
 console.log(getArea('trape', 10,5));
 
 console.log(getArea('trape', 10,5, 12));
+
+//요구사항 2
+//2.1. 다음처럼 동작하는 원의 넓이를 계산하는 함수
+// calculateCircle(10) 인경우 정상적인 결과를 출력.
+// calculateCircle("10") 인경우 
+//> "숫자형타입만 계산이 가능합니다."
+// calculateCircle() 인경우 
+//> "최소 한가지 값이 필요합니다"
+// calculateCircle(-1) 인경우 
+//> "반지름은 0보다 커야 합니다"
+console.log(calculateCircleSize(10));
+console.log(calculateCircleSize('10'));
+console.log(calculateCircleSize());
+console.log(calculateCircleSize(-1));
 
