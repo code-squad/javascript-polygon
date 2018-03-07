e5841e302b2d759566a121a13f42cfecf0a92b4e
Log91 <>
2018-03-07 17:32:53 +0900

    다각형 넓이 구하기

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 //공식 사용하기: 넓이 = π x 반지름^2 (넓이=파이 x 반지름 제곱)
 
+var array = [];
+
 function calculateCircle(radius) {
-    if (arguments.length < 1) return '최소 한 가지 값이 필요합니다';
+    var count = 0;
     if (!isNumber(radius)) return '숫자형타입만 계산이 가능합니다';
     if (!isPositiveNumber(radius)) return '반지름은 0보다 커야 합니다';
+    array.push('circle');
     return radius * radius * Math.PI;
 }
 
 function calculateRect(width, height) {
-    if (arguments.length < 1) return '최소 한 가지 값이 필요합니다';
     if (!isPositiveNumber(width, height)) return '밑변과 길이는 0보다 커야 합니다';
+    array.push('rect');
     return width * height;
 }
 
 function calculateTrapezoid(top, bottom, height) {
-    if (arguments.length < 1) return '최소 한 가지 값이 필요합니다';
-    if (arguments.length < 3) return '3개의 인자가 필요합니다';
     if (!isNumber(top, bottom, height)) return '숫자형 타입만 계산이 가능합니다.'
     if (!isPositiveNumber(top, bottom, height)) return '윗변, 아랫변, 높이는 0보다 커야 합니다';
+    array.push('trape');
     return (top + bottom) * height / 2;
 }
 
@@ -38,24 +40,28 @@ function isPositiveNumber() {
 }
 
 function getArea() {
+    if (arguments.length < 2) return '최소 한 가지 값이 필요합니다'
     switch (arguments[0]) {
         case 'circle':
-            if (arguments.length < 2) return '최소 한 가지 값이 필요합니다'
-            return calculateCircle(arguments[1]);
+            return calculateCircle(arguments[1]);;
             break;
         case 'rect':
-            if (arguments.length < 2) return '최소 한 가지 값이 필요합니다'
             if (arguments.length < 3) return '밑변과 길이 둘 다 입력해야 합니다'
             return calculateRect(arguments[1], arguments[2]);
             break;
         case 'trapezoid':
-            if (arguments.length < 2) return '최소 한 가지 값이 필요합니다'
             if (arguments.length < 4) return '3개의 인자가 필요합니다'
             return calculateTrapezoid(arguments[1], arguments[1], arguments[3])
             break;
     }
 }
 
+function getReport() {
+    console.log(array);
+}
 console.log(getArea('circle', 5));
-console.log(getArea('rect', 5, 2));
+console.log(getArea('circle', 5));
+console.log(getArea('circle', 5));
+console.log(getArea('rect', 2, 4));
 console.log(getArea('trapezoid', 1, 4, 2));
+getReport();
