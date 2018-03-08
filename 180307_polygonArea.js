82ff4b5a90a0d8093c03691da49815afff3cc00c
codesquad <>
2018-03-08 19:33:09 +0900

    수정(1차). 다각형의 넓이 함수

--- 180307_polygonArea.js
+++ 180307_polygonArea.js
@@ -1,29 +1,39 @@
 function circleArea(radius){
     if(arguments.length !== 1){
-        console.log("하나의 인자가 필요합니다");
-        return;
+        console.log("한 개의 인자가 필요합니다");
+    } else if(typeof radius !== "number"){
+        console.log("숫자형타입만 계산이 가능합니다");
+    } else if(radius <= 0){
+        console.log("반지름은 0보다 커야합니다");
+    } else {
+        console.log(Math.pow(radius, 2) * Math.PI);
     }
-    return Math.pow(radius, 2) * Math.PI;
 }
 
-function rectangleArea(){
-    switch(arguments.length){
-        case 1: return Math.pow(arguments[0],2);
-        case 2: return arguments[0] * arguments[1];
-        default:
-            console.log("하나 또는 두 개의 인자가 필요합니다");
-            return;
+function rectangleArea(bottom, height){
+    if(arguments.length !== 2){
+        console.log("두 개의 인자가 필요합니다");
+    } else if(typeof bottom !== "number" || typeof height !== "number"){
+        console.log("숫자형타입만 계산이 가능합니다");
+    } else if(bottom <= 0 || height <= 0){
+        console.log("인자는 0보다 커야합니다");
+    } else {
+        console.log(bottom * height);
     }
 }
 
 function trapezoidArea(bottom, top, height){
     if(arguments.length !== 3){
-        console.log("3개의 인자가 필요합니다");
-        return;
+        console.log("세 개의 인자가 필요합니다");
+    } else if(typeof bottom !== "number" || typeof top !== "number" || typeof height !== "number"){
+        console.log("숫자형타입만 계산이 가능합니다");
+    } else if(bottom <= 0 || top <= 0 || height <= 0){
+        console.log("인자는 0보다 커야합니다");
+    } else {
+        console.log(((bottom + top) * height)/2);
     }
-    return ((bottom + top) * height)/2;
 }
 
-console.log(circleArea(10));
-console.log(rectangleArea(10,15));
-console.log(trapezoidArea(10,5,12));
+circleArea(10);
+rectangleArea(15,10);
+trapezoidArea(10,5,12);
