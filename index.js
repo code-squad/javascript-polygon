781ad457ab77d8779ef374977666ab4f2b4c8eed
HYEON(mac) <antaehyeon@live.co.kr>
2018-03-08 18:44:38 +0900

    다각형의 넓이 중 파라미터를 체크하는 다각형의 넓이함수 첫번째 코드 작성

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // 원 넓이 구하는 함수
+var notNumberPattern = /D/;
+var re = new RegExp("[a-z0-9]");
+
 function calCircleArea (radius) {
-    return Math.PI * radius * radius;
+    if (arguments.length === 0)
+        return "최소 한가지 값이 필요합니다";
+
+    if (arguments[0] < 0)
+        return "반지름은 0보다 커야 합니다";
+    
+    if (typeof(radius) === "number")
+        return Math.PI * radius * radius;
+    else
+        return "숫자형 타입만 계산이 가능합니다";
 }
 
 // 사각형 넓이 구하는 함수
@@ -16,7 +28,14 @@ function calTrapezoidArea(lowerbase, upperbase, height) {
     return (lowerbase + upperbase) * height;
 }
 
+// 정상값 테스트
 console.log(calCircleArea(10));
+
+// 예외처리 테스트
+console.log(calCircleArea("10"));
+console.log(calCircleArea());
+console.log(calCircleArea(-1));
+
 console.log(calQuadRangleArea(10, 15));
 console.log(calTrapezoidArea(10, 5, 12));
 console.log(calTrapezoidArea(10, 5));
