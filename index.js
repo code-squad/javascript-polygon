0bab083531e02bda1d603f4ea3545157848d70e1
HYEON(win) <antaehyeon@live.co.kr>
2018-03-12 14:50:18 +0900

    [Fix] 다각형의 넓이 코드에 대한 피드백을 반영한다
    - if/else 를 사용할 때 괄호를 사용
    - 변수들을 중간에 선언하지말고, 최상위나 조건문 위쪽에 선언
    - 비교문 에러 수정
    - ES5/ES6 구문확인
    - isAllNumber 함수 생성

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ var calledFunctionCount = 0;
 var calledFunctionSequence = [];
 
 // 원 넓이 구하는 함수
-function calCircleArea (radius) {
+function calCircleArea(radius) {
+
+    let lastData = arguments[1];
+
     calledFunctionCount++;
     calledFunctionSequence.push('Circle');
-    if (arguments.length === 0)
+    if (arguments.length === 0) {
         return "최소 한가지 값이 필요합니다";
+    }
     else if (arguments.length === 2 && arguments[1] !== undefined) {
         for (let i = arguments[0]; i < arguments[1]; i++) {
             console.log(i + "번째 원의 넓이 : " + Math.PI * i * i);
             calledFunctionCount++;
             console.log("계산이 " + calledFunctionCount + "번 일어났습니다.");
         }
-        let lastData = arguments[1];
         return lastData + "번째 원의 넓이 : " + Math.PI * lastData * lastData;
     }
 
     if (arguments[0] < 0)
         return "반지름은 0보다 커야 합니다";
-    
-    if (typeof(radius) === "number")
+
+    if (isAllNumber(radius))
         return Math.PI * radius * radius + "\n" + "계산이 " + calledFunctionCount + "번 일어났습니다.";
     else
         return "숫자형 타입만 계산이 가능합니다";
@@ -38,10 +41,34 @@ function calRectangleArea(width, height) {
     if (arguments[0] <= 0 || arguments[1] <= 0)
         return "가로 및 세로 길이는 0보다 커야합니다";
 
-    if (typeof(width) === "number" && typeof(height) === "number")
+    if (isAllNumber(width, height)) {
         return width * height + "\n" + "계산이 " + calledFunctionCount + "번 일어났습니다.";
-    else
+    } else {
         return "숫자형 타입만 계산이 가능합니다";
+    }
+}
+
+function isAllNumber() {
+    var correctNum = 0;
+
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof (arguments[i]) === "number") {
+            correctNum++;
+        }
+    }
+
+    // ERROR
+    // for in 으로 받으면 모두 string 으로 type이 결정되어버림
+    // 새로 리턴을 하기 때문에 문제가 생기는 것 같음
+    // for (var value in arguments) {
+    //     var type = typeof (value);
+    //     console.log(type);
+    //     if (type === "number") { correctNum++; }
+    // }
+
+    var result = ((arguments.length) === correctNum) ? true : false;
+
+    return result;
 }
 
 // 사다리꼴 넓이 구하는 함수
@@ -56,14 +83,14 @@ function calTrapezoidArea(lowerbase, upperbase, height) {
     if (arguments[0] <= 0 || arguments[1] <= 0 || arguments[2] <= 0)
         return "밑변 및 윗변, 높이의 길이는 0보다 커야합니다";
 
-    if (typeof(lowerbase) === "number" && typeof(upperbase) && typeof(height) === "number")
+    if (isAllNumber(lowerbase, upperbase, height))
         return (lowerbase + upperbase) * height + "\n" + "계산이 " + calledFunctionCount + "번 일어났습니다.";
     else
         return "숫자형 타입만 계산이 가능합니다";
 }
 
 function getArea(mode, firstData, secondData, thirdData) {
-    switch(mode) {
+    switch (mode) {
         case "circle":
             console.log(calCircleArea(firstData, secondData));
             break;
@@ -82,42 +109,55 @@ function getReport() {
     calledFunctionSequence.forEach(value => console.log(value));
 }
 
-// 정상값 테스트
+// // 정상값 테스트
+// console.log(calCircleArea(10));
+//
+// // 예외처리 테스트
+// console.log(calCircleArea("10"));
+// console.log(calCircleArea());
+// console.log(calCircleArea(-1));
+//
+// console.log("-----------------");
+//
+// // 정상값 테스트
+// console.log(calRectangleArea(10, 15));
+//
+// // 예외처리 테스트
+// console.log(calRectangleArea(10));
+// console.log(calRectangleArea(10, 20, 30));
+// console.log(calRectangleArea(10, 0));
+// console.log(calRectangleArea(0, 10));
+// console.log(calRectangleArea("10", 15));
+// console.log(calRectangleArea(10, "15"));
+//
+// console.log("-----------------");
+//
+// console.log(calTrapezoidArea(10, 5, 12));
+// console.log(calTrapezoidArea(10, 5));
+// console.log(calTrapezoidArea(10, 20, 30, 40));
+// console.log(calTrapezoidArea("10", 10, 10));
+// console.log(calTrapezoidArea(10, 0, 10));
+//
+// console.log("-----------------");
+//
+// getArea('circle', 10);
+// getArea('rect', 10, 15);
+// getArea('trapezoid', 10, 5, 12);
+// getArea('circle', 1, 100);
+//
+// console.log("-----------------");
+//
+// getReport();
+
+
+// isAllNumber 테스트
 console.log(calCircleArea(10));
-
-// 예외처리 테스트
 console.log(calCircleArea("10"));
-console.log(calCircleArea());
-console.log(calCircleArea(-1));
 
-console.log("-----------------");
-
-// 정상값 테스트
 console.log(calRectangleArea(10, 15));
-
-// 예외처리 테스트
-console.log(calRectangleArea(10));
-console.log(calRectangleArea(10, 20, 30));
-console.log(calRectangleArea(10, 0));
-console.log(calRectangleArea(0, 10));
 console.log(calRectangleArea("10", 15));
-console.log(calRectangleArea(10, "15"));
-
-console.log("-----------------");
 
 console.log(calTrapezoidArea(10, 5, 12));
-console.log(calTrapezoidArea(10, 5));
-console.log(calTrapezoidArea(10, 20, 30, 40));
 console.log(calTrapezoidArea("10", 10, 10));
-console.log(calTrapezoidArea(10, 0, 10));
 
-console.log("-----------------");
 
-getArea('circle', 10);
-getArea('rect', 10, 15);
-getArea('trapezoid', 10, 5, 12);
-getArea('circle', 1, 100);
-
-console.log("-----------------");
-
-getReport();
