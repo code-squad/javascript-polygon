de1d536f23fe09e48170a5d7ae2c48b431d680b7
codesquad <>
2018-03-13 14:39:51 +0900

    Change code using throw and return
    throw와 return을 사용해 코드를  수정

--- 180307_polygonArea.js
+++ 180307_polygonArea.js
@@ -2,76 +2,82 @@ function circleArea(){
     switch(arguments.length){
         case 1:
             if(typeof arguments[0] !== "number"){
-                console.log("숫자형 타입만 계산이 가능합니다");
+                throw "숫자형 타입만 계산이 가능합니다";
             }else if(arguments[0] <= 0){
-                console.log("인자는 0보다 커야합니다");
+                throw "인자는 0보다 커야합니다";
             }else{
-                console.log(Math.pow(arguments[0], 2) * Math.PI);
+                let result = arguments[0] * arguments[0] * Math.PI;
+                return result;
             }
             break;
         case 2:
             if(typeof arguments[0] !== "number" || typeof arguments[1] !== "number"){
-                console.log("숫자형 타입만 계산이 가능합니다");
+                throw "숫자형 타입만 계산이 가능합니다";
             }else if(arguments[0] <= 0 || arguments[1] <= 0){
-                console.log("인자는 0보다 커야합니다");
+                throw "인자는 0보다 커야합니다";
             }else{
+                let result = [];
                 for(let i = arguments[0]; i <= arguments[1]; i++){
-                    console.log(i * i * Math.PI);
+                    result.push(i * i * Math.PI);
                 }
+                return result;
             }
             break;
         default :
-            console.log("한 개 또는 두 개의 인자가 필요합니다");
+            throw "한 개 또는 두 개의 인자가 필요합니다";
     }
 }
 
 function rectArea(bottom, height){
     if(arguments.length !== 2){
-        console.log("두 개의 인자가 필요합니다");
+        throw "두 개의 인자가 필요합니다";
     } else if(typeof bottom !== "number" || typeof height !== "number"){
-        console.log("숫자형 타입만 계산이 가능합니다");
+        throw "숫자형 타입만 계산이 가능합니다";
     } else if(bottom <= 0 || height <= 0){
-        console.log("인자는 0보다 커야합니다");
+        throw "인자는 0보다 커야합니다";
     } else {
-        console.log(bottom * height);
+        let result = bottom * height;
+        return result;
     }
 }
 
 function trapezoidArea(bottom, top, height){
     if(arguments.length !== 3){
-        console.log("세 개의 인자가 필요합니다");
+        throw "세 개의 인자가 필요합니다";
     } else if(typeof bottom !== "number" || typeof top !== "number" || typeof height !== "number"){
-        console.log("숫자형 타입만 계산이 가능합니다");
+        throw "숫자형 타입만 계산이 가능합니다";
     } else if(bottom <= 0 || top <= 0 || height <= 0){
-        console.log("인자는 0보다 커야합니다");
+        throw "인자는 0보다 커야합니다";
     } else {
-        console.log(((bottom + top) * height)/2);
+        result = ((bottom + top) * height)/2;
+        return result;
     }
 }
 
 let getArea = (function(){
     let log = [];
     return function(shape, ...args){
+        let result;
         switch(shape){
             case 'circle':
-                circleArea(...args);
+                result = circleArea(...args);
                 log.push('circle');
                 break;
             case 'rect':
-                rectArea(...args);
+                result = rectArea(...args);
                 log.push('rect');
                 break;
             case 'trapezoid':
-                trapezoidArea(...args);
+                result = trapezoidArea(...args);
                 log.push('trapezoid');
                 break;
             case 'log':
                 return log.join();
             default :
-                console.log("올바른 인자를 입력하세요");
-                return ;
+                throw "올바른 인자를 입력하세요";
         }
         console.log("계산이 " + log.length + "번 일어났습니다.");
+        return result;
     }
 })();
 
@@ -79,7 +85,7 @@ let getReport = function(){
     console.log("계산수행순서 : " + getArea('log'));
 }
 
-getArea('circle', 10, 12);
-getArea('rect', 10, 15);
-getArea('trapezoid', 10, 15, 12);
+console.log(getArea('circle', 10, 12));
+console.log(getArea('rect', 10, 15));
+console.log(getArea('trapezoid', 10, 15, 12));
 getReport();
