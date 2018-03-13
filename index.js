cbd4c42e5b1b8ef76e35041af46adb034939ba7b
HYEON(win) <antaehyeon@live.co.kr>
2018-03-13 16:36:45 +0900

    [Fix] 다각형의 넓이 코드에 대한 피드백을 반영한다
    - 오류상황은 함수초반에 모아서 바로 return
    - 코드 작성할 때 더 간단한 방법은 없는지 생각하면서 코딩하기

--- index.js
+++ index.js
@@ -4,29 +4,33 @@ var calledFunctionSequence = [];
 // 원 넓이 구하는 함수
 function calCircleArea(radius) {
 
-    let lastData = arguments[1];
+    var lastData = arguments[1];
 
     calledFunctionCount++;
     calledFunctionSequence.push('Circle');
+
     if (arguments.length === 0) {
         return "최소 한가지 값이 필요합니다";
     }
-    else if (arguments.length === 2 && arguments[1] !== undefined) {
-        for (let i = arguments[0]; i < arguments[1]; i++) {
-            console.log(i + "번째 원의 넓이 : " + Math.PI * i * i);
-            calledFunctionCount++;
-            console.log("계산이 " + calledFunctionCount + "번 일어났습니다.");
-        }
-        return lastData + "번째 원의 넓이 : " + Math.PI * lastData * lastData;
+
+    if (arguments[0] < 0) {
+        return "반지름은 0보다 커야 합니다";
     }
 
-    if (arguments[0] < 0)
-        return "반지름은 0보다 커야 합니다";
-
-    if (isAllNumber(radius))
-        return Math.PI * radius * radius + "\n" + "계산이 " + calledFunctionCount + "번 일어났습니다.";
-    else
+    if (!isAllNumber(radius)) {
         return "숫자형 타입만 계산이 가능합니다";
+    }
+
+    if (arguments.length === 2 && lastData !== undefined) {
+        for (let i = arguments[0]; i < lastData; i++) {
+            log(i + "번째 원의 넓이 : " + Math.PI * i * i);
+            calledFunctionCount++;
+            log("계산이 " + calledFunctionCount + "번 일어났습니다.");
+        }
+        return lastData + "번째 원의 넓이 : " + Math.PI * lastData * lastData;
+    } else {
+        return Math.PI * radius * radius + "\n" + "계산이 " + calledFunctionCount + "번 일어났습니다.";
+    }
 }
 
 // 사각형 넓이 구하는 함수
@@ -49,26 +53,23 @@ function calRectangleArea(width, height) {
 }
 
 function isAllNumber() {
-    var correctNum = 0;
 
     for (var i = 0; i < arguments.length; i++) {
-        if (typeof (arguments[i]) === "number") {
-            correctNum++;
+        if (typeof (arguments[i]) !== "number") {
+            return false;
         }
     }
+
+    return true;
 
     // ERROR
     // for in 으로 받으면 모두 string 으로 type이 결정되어버림
     // 새로 리턴을 하기 때문에 문제가 생기는 것 같음
     // for (var value in arguments) {
     //     var type = typeof (value);
-    //     console.log(type);
+    //     log(type);
     //     if (type === "number") { correctNum++; }
     // }
-
-    var result = ((arguments.length) === correctNum) ? true : false;
-
-    return result;
 }
 
 // 사다리꼴 넓이 구하는 함수
@@ -92,13 +93,13 @@ function calTrapezoidArea(lowerbase, upperbase, height) {
 function getArea(mode, firstData, secondData, thirdData) {
     switch (mode) {
         case "circle":
-            console.log(calCircleArea(firstData, secondData));
+            log(calCircleArea(firstData, secondData));
             break;
         case "rect":
-            console.log(calRectangleArea(firstData, secondData));
+            log(calRectangleArea(firstData, secondData));
             break;
         case "trapezoid":
-            console.log(calTrapezoidArea(firstData, secondData, thirdData));
+            log(calTrapezoidArea(firstData, secondData, thirdData));
             break;
         default:
             break;
@@ -106,58 +107,74 @@ function getArea(mode, firstData, secondData, thirdData) {
 }
 
 function getReport() {
-    calledFunctionSequence.forEach(value => console.log(value));
+    calledFunctionSequence.forEach(value => log(value));
+}
+
+function log(data) {
+    console.log(data);
 }
 
 // // 정상값 테스트
-// console.log(calCircleArea(10));
+// log(calCircleArea(10));
 //
 // // 예외처리 테스트
-// console.log(calCircleArea("10"));
-// console.log(calCircleArea());
-// console.log(calCircleArea(-1));
+// log(calCircleArea("10"));
+// log(calCircleArea());
+// log(calCircleArea(-1));
 //
-// console.log("-----------------");
+// log("-----------------");
 //
 // // 정상값 테스트
-// console.log(calRectangleArea(10, 15));
+// log(calRectangleArea(10, 15));
 //
 // // 예외처리 테스트
-// console.log(calRectangleArea(10));
-// console.log(calRectangleArea(10, 20, 30));
-// console.log(calRectangleArea(10, 0));
-// console.log(calRectangleArea(0, 10));
-// console.log(calRectangleArea("10", 15));
-// console.log(calRectangleArea(10, "15"));
+// log(calRectangleArea(10));
+// log(calRectangleArea(10, 20, 30));
+// log(calRectangleArea(10, 0));
+// log(calRectangleArea(0, 10));
+// log(calRectangleArea("10", 15));
+// log(calRectangleArea(10, "15"));
 //
-// console.log("-----------------");
+// log("-----------------");
 //
-// console.log(calTrapezoidArea(10, 5, 12));
-// console.log(calTrapezoidArea(10, 5));
-// console.log(calTrapezoidArea(10, 20, 30, 40));
-// console.log(calTrapezoidArea("10", 10, 10));
-// console.log(calTrapezoidArea(10, 0, 10));
+// log(calTrapezoidArea(10, 5, 12));
+// log(calTrapezoidArea(10, 5));
+// log(calTrapezoidArea(10, 20, 30, 40));
+// log(calTrapezoidArea("10", 10, 10));
+// log(calTrapezoidArea(10, 0, 10));
 //
-// console.log("-----------------");
+// log("-----------------");
 //
 // getArea('circle', 10);
 // getArea('rect', 10, 15);
 // getArea('trapezoid', 10, 5, 12);
 // getArea('circle', 1, 100);
 //
-// console.log("-----------------");
+// log("-----------------");
 //
 // getReport();
 
 
 // isAllNumber 테스트
-console.log(calCircleArea(10));
-console.log(calCircleArea("10"));
+log(calCircleArea(10));
+log(calCircleArea("10"));
 
-console.log(calRectangleArea(10, 15));
-console.log(calRectangleArea("10", 15));
+log(calRectangleArea(10, 15));
+log(calRectangleArea("10", 15));
 
-console.log(calTrapezoidArea(10, 5, 12));
-console.log(calTrapezoidArea("10", 10, 10));
+log(calTrapezoidArea(10, 5, 12));
+log(calTrapezoidArea("10", 10, 10));
+
+// 2차 테스트
+getArea('circle', 1, 100);
+
+// 정상값 테스트
+log(calCircleArea(10));
+log(calCircleArea(10));
+
+// 예외처리 테스트
+log(calCircleArea("10"));
+log(calCircleArea());
+log(calCircleArea(-1));
 
 
