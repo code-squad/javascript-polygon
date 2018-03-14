6d842574aebab6922646529eae929061492a75fc
juchan96 <ghkdwncks96@gmail.com>
2018-03-14 12:31:28 +0900

    calculateCircle #2

--- calculateCircle.js
+++ calculateCircle.js
@@ -1,28 +1,71 @@
-/*
-calculateCircle(10) 인경우 정상적인 결과를 출력.
-
-calculateCircle("10") 인경우 
-> "숫자형타입만 계산이 가능합니다."
-
-calculateCircle() 인경우 
-> "최소 한가지 값이 필요합니다"
-
-calculateCircle(-1) 인경우 
-> "반지름은 0보다 커야 합니다"
-*/
-
-function calculateCircle(radius){
+function checkLogic(){
+    for(let n = 0; n < arguments.length; n++){
+      if(typeof arguments[n] === "string"){
+        return "숫자형 타입만 계산이 가능합니다.";
+      }
+      else if(arguments[n] <= 0){
+        return "길이가 0보다 커야 합니다.";
+      }
+    }
+      if(arguments.length === 0){
+        return "최소 1개의 값이 필요합니다";
+      }
+  }
+  
+  function getWidthCircle(radius){
+    if(typeof radius === "number"){
     let result = radius * radius * Math.PI;
-    console.log(typeof radius);
-    if((typeof radius) === String){
-        console.log("숫자형 타입만 계산이 가능합니다.");
+    return result;
     }
-    else if(arguments.length === 0){
-        console.log("최소 한 가지 값이 필요합니다");
+    else{
+    return checkLogic(radius);
     }
-    else if(radius <= 0){
-        console.log("반지름이 0보다 커야 합니다.");
+  }
+  
+  function getWidthSquare(base, height){
+    if(arguments.length <= 1){
+      return "2개의 인자값이 필요합니다";
     }
+    let arr = [];
+    for(let n = 0; n < arguments.length; n++){
+      if(typeof arguments[n] === "number"){
+        arr.push(n);
+      }
+      else{
+        break;
+      }
+    }
+    if(arr.length === arguments.length){
+      let result = base * height;
+      return result;
+    }
+    else{
+      return checkLogic(base, height);
+    }
+  }
+  
+  function getWidthTrapezoid(base, uppbase, height){
+    if(arguments.length <= 2){
+      return "3개의 인자값이 필요합니다";
+    }
+    let arr = [];
+    for(let n = 0; n < arguments.length; n++){
+      if(typeof arguments[n] === "number"){
+        arr.push(n);
+      }
+      else{
+        break;
+      }
+    }
+     if(arr.length === arguments.length){
+        let result = (base + uppbase) * height * 0.5;
         return result;
-}
-console.log(calculateCircle(50));
+      }
+    else{
+      return checkLogic(base, uppbase, height);
+    }
+  }
+  
+  console.log(getWidthCircle(10));
+  console.log(getWidthSquare(10,5));
+  console.log(getWidthTrapezoid(10,5,12));
