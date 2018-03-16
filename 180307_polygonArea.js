d418f56f90ab1de6886124fdf5416bf4b15f7c28
youngdo212 <nsa06031@gmail.com>
2018-03-16 12:06:01 +0900

    Refactor functions
    기능을 최소화 한 함수들로 만듬
    에러체크를 checkError함수로 통일시킴
    원의 넓이 구하는 함수를 인자 1개와 인자 2개일 때로 함수 분할
    분할 된 원 넓이 함수를 getCircleArea에 넣어줌

--- 180307_polygonArea.js
+++ 180307_polygonArea.js
@@ -1,60 +1,37 @@
-function circleArea(){
-    switch(arguments.length){
-        case 1:
-            if(typeof arguments[0] !== "number"){
-                throw "숫자형 타입만 계산이 가능합니다";
-            }else if(arguments[0] <= 0){
-                throw "인자는 0보다 커야합니다";
-            }else{
-                let result = arguments[0] * arguments[0] * Math.PI;
-                return result;
-            }
-            break;
-        case 2:
-            if(typeof arguments[0] !== "number" || typeof arguments[1] !== "number"){
-                throw "숫자형 타입만 계산이 가능합니다";
-            }else if(arguments[0] <= 0 || arguments[1] <= 0){
-                throw "인자는 0보다 커야합니다";
-            }else{
-                let result = [];
-                for(let i = arguments[0]; i <= arguments[1]; i++){
-                    result.push(i * i * Math.PI);
-                }
-                return result;
-            }
-            break;
-        default :
-            throw "한 개 또는 두 개의 인자가 필요합니다";
-    }
+function checkError(arr, ...validLength){
+    if(!validLength.includes(arr.length)) throw "올바른 인자 개수를 입력하십시오";
+    if(!arr.every((element) => typeof element === "number")) throw "숫자형 타입만 계산이 가능합니다";
+    if(!arr.every((element) => element > 0)) throw "인자는 0보다 커야합니다";
 }
 
-function rectArea(bottom, height){
-    if(arguments.length !== 2){
-        throw "두 개의 인자가 필요합니다";
-    } else if(typeof bottom !== "number" || typeof height !== "number"){
-        throw "숫자형 타입만 계산이 가능합니다";
-    } else if(bottom <= 0 || height <= 0){
-        throw "인자는 0보다 커야합니다";
-    } else {
-        let result = bottom * height;
-        return result;
-    }
-    logNum ++;
-    logArr.push('rect');
-    console.log("계산이 "+logNum+"번 일어났습니다");
+function getACircleArea(radius){
+    return Math.pow(radius, 2) * Math.PI;
 }
 
-function trapezoidArea(bottom, top, height){
-    if(arguments.length !== 3){
-        throw "세 개의 인자가 필요합니다";
-    } else if(typeof bottom !== "number" || typeof top !== "number" || typeof height !== "number"){
-        throw "숫자형 타입만 계산이 가능합니다";
-    } else if(bottom <= 0 || top <= 0 || height <= 0){
-        throw "인자는 0보다 커야합니다";
-    } else {
-        result = ((bottom + top) * height)/2;
-        return result;
+function getManyCircleArea(start, end){
+    let result = [];
+    for(let radius = start; radius <= end; radius++){
+        result.push(getACircleArea(radius));
     }
+    return result;
+}
+
+function getCircleArea(){
+    checkError(Array.from(arguments), 1, 2);
+    if(arguments.length === 1) return getACircleArea(...arguments);
+    if(arguments.length === 2) return getManyCircleArea(...arguments);
+}
+
+function getRectArea(bottom, height){
+    checkError(Array.from(arguments), 2);
+    let result = bottom * height;
+    return result;
+}
+
+function getTrapezoidArea(bottom, top, height){
+    checkError(Array.from(arguments), 3);
+    result = ((bottom + top) * height)/2;
+    return result;
 }
 
 let getArea = (function(){
@@ -63,15 +40,15 @@ let getArea = (function(){
         let result;
         switch(shape){
             case 'circle':
-                result = circleArea(...args);
+                result = getCircleArea(...args);
                 log.push('circle');
                 break;
             case 'rect':
-                result = rectArea(...args);
+                result = getRectArea(...args);
                 log.push('rect');
                 break;
             case 'trapezoid':
-                result = trapezoidArea(...args);
+                result = getTrapezoidArea(...args);
                 log.push('trapezoid');
                 break;
             case 'log':
@@ -79,7 +56,7 @@ let getArea = (function(){
             default :
                 throw "올바른 인자를 입력하세요";
         }
-        console.log("계산이 " + log.length + "번 일어났습니다.");
+        console.log("함수 호출이 " + log.length + "번 일어났습니다.");
         return result;
     }
 })();
