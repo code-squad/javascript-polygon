9d90ff3231e14b99292f4bddd85579e1b22661f6
juchan96 <ghkdwncks96@gmail.com>
2018-03-20 16:09:15 +0900

    line41 if 중첩문 제거

--- getArea.js
+++ getArea.js
@@ -1,17 +1,17 @@
-function checkLogic(){
+function getcheckerror(){
     for(let n = 0; n < arguments.length; n++){
       if(typeof arguments[n] !== "number"){
         return "숫자형 타입만 계산이 가능합니다.";
       }
       else if(arguments[n] <= 0){
         return "길이가 0보다 커야 합니다.";
-      }
+      } 
     }
     if(arguments.length === 0){
       return "정확한 인자의 갯수를 입력하세요";
     }
-    return "넓이를 출력";
-  }
+    return true;
+}
   
   //원의 넓이
   function getWidthCircle(radius){
@@ -33,35 +33,32 @@ function checkLogic(){
 
 
 function getArea(polygon,...args){
-  let check = checkLogic(...args);
-  if(check !== "넓이를 출력"){
+  let check = getcheckerror(...args);
+  if(check !== true){
     return check;
-  }else{
-    if(polygon === 'circle'){
-      if(arguments.length === 2){
-        let radius = arguments[1];    
-        return getWidthCircle(radius);
-      }
-      else if(arguments.length === 3){
-        let arr = [];
-        for(let radius = arguments[1]; radius <= arguments[2]; radius ++){
-            arr.push(getWidthCircle(radius));
-        }
-        return arr;
-      }
+  }
+  
+  if(polygon === 'circle'){
+    let arr = [];
+    for(let radius = args[0]; radius <= args[1]; radius ++){
+      arr.push(getWidthCircle(radius));
     }
-    else if(polygon === 'rect' && arguments.length === 3){
-      let base = arguments[1], height = arguments[2];
-      return getWidthSquare(base, height);
+    if(typeof args[1] !== "number"){
+      return getWidthCircle(args[0]);
     }
-    else if(polygon === 'trapezoid' && arguments.length === 4){
-      let base = arguments[1], uppbase = arguments[2], height = arguments[3];
-      return getWidthTrapezoid(base, uppbase, height);
-    }
+    return arr;
+  }
+  else if(polygon === 'rect'){
+    let base = args[0], height = args[1];
+    return getWidthSquare(base, height);
+  }
+  else if(polygon === 'trapezoid'){
+    let base = args[0], uppbase = args[1], height = args[2];
+    return getWidthTrapezoid(base, uppbase, height);
   }
 }
   
-console.log(getArea('circle',-1));
+console.log(getArea('circle',1));
 console.log(getArea('rect', 10,15));
 console.log(getArea('trapezoid',10,15,12));
-console.log(getArea('circle', 1,3));
+console.log(getArea('circle', 1,5));
