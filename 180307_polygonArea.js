3be19d4d1df73f1b01febc61b49ea254dc8bc56e
youngdo212 <nsa06031@gmail.com>
2018-03-22 18:35:38 +0900

    Create includes method for compatibillity
    includes()메소드의 호환성을 위해 새로 정의

--- 180307_polygonArea.js
+++ 180307_polygonArea.js
@@ -1,3 +1,38 @@
+if(!Array.prototype.includes){
+    Object.defineProperty(Array.prototype, 'includes', {
+        value : function(searchElement, fromIndex){
+            if(this === null){
+                throw new TypeError('"this" is null or not defined'); //배열이 null일 때 타입에러
+            }
+
+            var o = Object(this); //의미 모름
+
+            var len = o.length >>> 0; //의미 모름
+
+            if(len === 0){
+                return false;
+            }
+
+            var n = fromIndex | 0; //default
+
+            var k = Math.max(n>=0 ? n : len - Math.abs(n), 0); //fromIndex < 0 일때 뒤에서 부터 셈, 배열의 길이를 넘어서면 0부터
+
+            function sameValueZero(x, y){
+                return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y)); //NaN일 경우도 세줌
+            }
+
+            while(k < len){
+                if(sameValueZero(o[k], searchElement)){
+                    return true;
+                }
+                k++;
+            }
+
+            return false;
+        }
+    });
+}
+
 function checkError(arr, ...validLength){
     if(!validLength.includes(arr.length)) throw "올바른 인자 개수를 입력하십시오";
     if(!arr.every((element) => typeof element === "number")) throw "숫자형 타입만 계산이 가능합니다";
