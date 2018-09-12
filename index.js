81658776f90d17038564ffb7a8dc3726831a8e37
sk2504 <sk2504@naver.com>
2018-09-12 15:23:10 +0900

    step1 다각형 넓이 구하기 커밋입니다.

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-function calculateCircleSize(radius1,radius2) {
-    var size = 0;
-    if(arguments.length === 1){
-        size = radius1 * radius1 * Math.PI;
-        console.log("원의 넓이: " + size);
-    }
-    if(arguments.length === 2){
-        for(var i = radius1; i <= radius2; i++){
-            console.log('반지름이 '+i+'인 원의 넓이는 :'+(i*i*Math.PI));
-        }
-    }
-}
-
-function calculateRectSize(width,height) {
-    if(typeof width !== 'number' && typeof height !== 'number'){
-        console.log('숫자형타입만 계산이 가능합니다');
-    } else if(arguments.length===0){
-        console.log('최소 한가지 값이 필요합니다')
-    } else if(width<= 0 || height<= 0){
-        console.log('길이값과 높이값은 0보다 커야합니다');
-    }
-    else {
-        var size = width * height / 2;
-        console.log("사각형의 넓이: " + size);
+// 1. 반지름을 입력받아 원의 넓이 계산
+function circlesizeCalc(radius) {
+    if (arguments.length === 0) {
+        console.log('반지름을 입력해주세요')
+    } else if (typeof radius !== 'number') {
+        console.log('숫자만 입력해주세요')
+    } else if (radius <= 0) {
+        console.log('반지름은 0보다 커야합니다')
+    } else {
+        var result = Math.PI * Math.pow(radius, 2);
+        console.log('원의넓이는' + result + '입니다');
     }
 }
-
-function calculateTrapeSize(upper,bottom,height) {
-    if(arguments.length<=2){
-        console.log('3개의 인자가 필요합니다');
-    }
-
-    var size = upper*bottom*height/2;
-    console.log("사다리꼴의 넓이: "+size);
-}
-
-
-
-function getArea(shape, para1, para2, para3) {
-    switch(shape){
-        case 'circle': calculateCircleSize(para1, para2);
-            break;
-        case 'rect': calculateRectSize(para1, para2);
-            break;
-        case 'trape': calculateTrapeSize(para1, para2, para3);
-            break;
+// 2. 사각형의 넓이 계산
+function areaSizecalc(base, height) {
+    if (arguments.length < 2) {
+        console.log('밑변의 길이와 높이를 입력해주세요')
+    } else if (typeof base !== 'number' || typeof height !== 'number') {
+        console.log('숫자만 입력해주세요')
+    } else if (base <= 0 || height <= 0) {
+        console.log('밑변과 높이는 0보다 커야합니다')
+    } else {
+        var result = base * height;
+        console.log('사각형의 넓이는' + result + '입니다');
     }
 }
-
-getArea('circle',1,10);
+// 3.사다리꼴의 넓이
+function trapezoidSizecalc(base1, base2, height) {
+    if (arguments.length < 3) {
+        console.log('밑변,윗변의 길이와 높이를 입력해주세요')
+    } else if (typeof base1 !== 'number' || typeof base2 !== 'number' || typeof height !== 'number') {
+        console.log('숫자만 입력해주세요')
+    } else if (base1 <= 0 || base2 <= 0 || height <= 0) {
+        console.log('밑변과 윗변과 높이는 0보다 커야합니다')
+    } else {
+        var result = (base1 + base2) / 2 * height;
+        console.log('사다리꼴의 넓이는' + result + '입니다');
+    }
+}
+// 4. 원기둥의 넓이 계산
+function trapezoidSizecalc(radius, height) {
+    if (arguments.length < 2) {
+        console.log('반지름과 높이를 입력해주세요')
+    } else if (typeof radius !== 'number' || typeof height !== 'number') {
+        console.log('숫자만 입력해주세요')
+    } else if (radius <= 0 || height <= 0) {
+        console.log('반지름과 높이는 0보다 커야합니다')
+    } else {
+        var result = 2 * Circle(radius) + (2 * Math.PI * radius * height);
+        console.log('사다리꼴의 넓이는' + result + '입니다');
+    }
+}
