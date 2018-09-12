63adc015b3edeefdc092013d834b5120c61c4cbe
sk2504 <sk2504@naver.com>
2018-09-12 17:06:43 +0900

    Step2 1-2 요구사항
    printExecutionSequence 함수를 만들어 로깅을 구현,
    실행 횟수기록 기능도 추가하였습니다

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// printExecutionSequence 함수 만들기
+let logFunctions = [];
+let countFunction = 0;
+function printExecutionSequence() {
+    console.log('계산 실행순서 : ' + logFunctions.join());
+    console.log('계산 실행 횟수 : ' + countFunction);
+}
+function pushLog(shape) {
+    logFunctions.push(shape);
+    countFunction++;
+}
 // 1. 반지름을 입력받아 원의 넓이 계산
-function circlesizeCalc(radius1, radius2) {
+function circleSizecalc(radius1, radius2) {
     if (arguments.length === 0) {
         console.log('반지름을 입력해주세요')
     } else if (typeof radius1 !== 'number') {
@@ -9,11 +20,13 @@ function circlesizeCalc(radius1, radius2) {
     } else if (arguments.length === 1) {
         var result = Math.PI * Math.pow(radius1, 2);
         console.log('원의 넓이는' + result + '입니다');
+        pushLog('circle')
         return result;
     } else {
         for (i = radius1; i <= radius2; i++) {
             var result = Math.PI * Math.pow(i, 2);
-            console.log('반지름' + i + '인 원의 넓이는' + result + '입니다')
+            console.log('반지름' + i + '인 원의 넓이는' + result + '입니다');
+            pushLog('circle');
         }
     }
 }
@@ -28,6 +41,7 @@ function rectCalc(base, height) {
     } else {
         var result = base * height;
         console.log('사각형의 넓이는' + result + '입니다');
+        pushLog('rect');
     }
 }
 // 3.사다리꼴의 넓이
@@ -41,6 +55,7 @@ function trapezoidSizecalc(base1, base2, height) {
     } else {
         var result = (base1 + base2) / 2 * height;
         console.log('사다리꼴의 넓이는' + result + '입니다');
+        pushLog('trapezoid');
     }
 }
 // 4. 원기둥의 겉넓이 계산
@@ -52,15 +67,17 @@ function CylinderSizecalc(radius, height) {
     } else if (radius <= 0 || height <= 0) {
         console.log('반지름과 높이는 0보다 커야합니다')
     } else {
-        var result = 2 * circlesizeCalc(radius) + (2 * Math.PI * radius * height);
-        console.log('원기둥의 겉넓이는' + result + '입니다');
+        var result = 2 * Math.PI * radius * (radius + height)
+        // 2 * circleSizecalc(radius) + (2 * Math.PI * radius * height);
+        console.log('원기둥의 겉넓이는 ' + result + '입니다');
+        pushLog('cylinder');
     }
 }
 
 // getArea 함수를 사용하면 모든도형의 넓이값을 얻는다.
 function getArea(shape, para1, para2, para3) {
     switch (shape) {
-        case 'circle': circlesizeCalc(para1, para2)
+        case 'circle': circleSizecalc(para1, para2)
             break;
         case 'rect': rectCalc(para1, para2)
             break;
@@ -70,8 +87,10 @@ function getArea(shape, para1, para2, para3) {
             break;
     }
 }
+
 getArea('circle', 5)
 getArea('circle', 5, 8);
 getArea('rect', 5, 10);
 getArea('trapezoid', 5, 10, 5);
 getArea('Cylinder', 5, 10);
+printExecutionSequence();
