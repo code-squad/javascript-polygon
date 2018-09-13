c7ce1b823f284af651bfcecd3f25cb6d8c29dbf5
Head <mrdoo309@gmail.com>
2018-09-13 10:02:18 +0900

    add function sizeSquare, add testcode

--- polygonSize.js
+++ polygonSize.js
@@ -1,15 +1,29 @@
 function sizeCircle(radius) {
-    if(toString.call(radius) != '[object Number]') {
-        return console.log('숫자만 계산 가능합니다.')
+    if (toString.call(radius) != '[object Number]') {
+        return console.log('숫자만 계산 가능합니다.');
     }
     const circleArea = radius * radius * Math.PI
     console.log('반지름이 ' + radius + '인 원의넓이 : ' + circleArea);
 }
-//testcode
+//test circle
 sizeCircle(4)
 sizeCircle('문자')
+sizeCircle();
 
-function sizeSquare(base, height) {
-    const squareArea = base * height
+function sizeSquare(lowerSide, height) {
+    if(lowerSide === undefined || height === undefined) {
+        return console.log('인자가 부족합니다.')
+    } else if (toString.call(lowerSide) != '[object Number]' || toString.call(height) != '[object Number]') {
+        return console.log('숫자만 계산 가능합니다.')
+    }
+    const squareArea = lowerSide * height
+    console.log('밑변과 아랫변이 ' + lowerSide + ', ' + height + '인 사각형의 넓이' + squareArea)
 }
+
+//test square
+sizeSquare(4, 3)
+sizeSquare(3)
+sizeSquare()
+sizeSquare('문자', '문자');
+sizeSquare('문자');
 
