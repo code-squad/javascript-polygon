4709a85d77317af988eac3ababdfb885316c355a
cohily12 <k96079123@gmail.com>
2018-09-13 15:45:54 +0900

    replaced funtion, used rest parameters and add printErro

--- lecture1_step1.js
+++ lecture1_step1.js
@@ -1,65 +1,69 @@
 // Step1 
 
+function isSame(fn, args){
+    return fn.length === args.length ? true : false;    
+}
+
+function isNum(...args){
+    args.forEach(function(val){
+        if(typeof val !== 'number') return false;
+    });
+    return true;
+}
+
+function printError(){
+    console.log('인자(숫자, 개수)를 확인해주세요');
+}
+
 // 원 넓이 구하기
-function circle(r){
-    if(isNum(...arguments)) {
-        if(arguments.length == 2){
-            for(let i = arguments[0]; i <= arguments[1]; i++){
-                console.log(circle(i));
-            }
-        } else if(isSame(circle, arguments)){
-            return r * r * Math.PI;
-        } 
+function circle(r1, r2){
+    if(arguments.length === 2 && isNum(...arguments)){
+        for(let i = r1; i <= r2; i++){
+            console.log(circle(i));
+        }
+    } else if(arguments.length === 1 && isNum(...arguments)){
+        return r1 * r1 * Math.PI;
+    } else {
+        printError();
     }
 }
 
 // 사각형 넓이 구하기
-function rect(w, h){
+function rect(w, h){  
     if(isSame(rect, arguments) && isNum(...arguments)) return w * h;
+    printError();
 }
 
 // 사다리꼴 넓이 구하기
 function trapezoid(a, b, h){
     if(isSame(trapezoid, arguments) && isNum(...arguments)) return (a+b) * h / 2;
+    printError();
 }
 
 // 원기둥 넓이 구하기
 function cylinder(r, h){
-    if(isNum(...arguments) && isSame(cylinder, arguments)) return circle(r) * h;
-}
-
-// 인자 type 확인
-function isNum(){
-    Array.prototype.forEach.call(arguments, function(val){
-        if(typeof val !== 'number') throw new Error('숫자를 입력해주세요');
-    })
-    return true;
-}
-
-// 인자 개수 확인
-function isSame(fn, args){
-    if(fn.length !== args.length) throw new Error('인자의 개수를 확인하세요.');
-    return true;
+    if(isSame(cylinder, arguments) && isNum(...arguments)) return circle(r) * h;
+    printError();
 }
 
 //Step2.1 getArea 함수
-function getArea(fig, a, b, c){
+function getArea(fig, ...args){
     let result;
         switch(fig){
             case 'circle':
-                result = circle(a, b);
+                result = circle(...args);
                 break;
             case 'rect':
-                result = rect(a, b);
+                result = rect(...args);
                 break;
             case 'trapezoid':
-                result = trapezoid(a,b,c);
+                result = trapezoid(...args);
                 break;
             case 'cyl':
-                result = cylinder(a,b);
+                result = cylinder(...args);
                 break;
             default :
                 console.log('도형을 입력을 해주세요.');
-        }       
-    return result;   
+        }
+    return result;          
 }
