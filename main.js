b4fedac37b540f08d7de552fb9304cb5bc3dd4e6
Boyoung Kim <boyoung@vanary.com>
2018-09-13 18:18:29 +0900

    Add new getArea method

--- main.js
+++ main.js
@@ -7,11 +7,21 @@ Requirements
     숫자가 아니면 에러를 반환하도록 구현한다.
     인자의갯수가 부족하면 에러를 반환한다.
 */
+
+/*
+
+getCircle() 
+getCircle() 
+getArea('circle',2) 
+getArea('rect',2,3) 
+printExecutionSequence()  //printExecutionSequence가 불려지면, 함수 호출된 순서를 출력한다. 
+> 계산수행순서 : circle, circle, circle, rect
+*/
 const computeArea = {
     circle(radius) {
         return Math.PI * Math.pow(radius, 2);
     },
-    square(width, height) {
+    rect(width, height) {
         return width * height;
     },
     trapezoid(top, bottom, height) {
@@ -29,8 +39,8 @@ const printArea = {
     circle(circleRadius, circleArea) {
         console.log(`입력하신 반지름 ${circleRadius}의 원 넓이는 ${circleArea}입니다.`);
     },
-    square(squareWidth, squareHeight, squareArea) {
-        console.log(`입력하신 너비 ${squareWidth}, 높이 ${squareHeight}의 사각형 넓이는 ${squareArea}입니다.`);
+    rect(rectWidth, rectHeight, rectArea) {
+        console.log(`입력하신 너비 ${rectWidth}, 높이 ${rectHeight}의 사각형 넓이는 ${rectArea}입니다.`);
     },
     trapezoid(trapeTop, trapeBottom, trapeHeight, trapeArea) {    
         console.log(`입력하신 윗변 ${trapeTop}, 아랫변 ${trapeBottom}, 높이 ${trapeHeight}의 사다리꼴 넓이는 ${trapeArea}입니다.`);
@@ -50,7 +60,7 @@ function checkInputErr(arr, rightNumOfArgs) {
         return arr.some((el) => (typeof el != "number")? true : false);
     }
     function hasLessElems(arr, rightNumOfArgs) {
-        if(typeof arr[0] === "undefined") {
+        if(!arr[0]) {
             console.log('아무 정보도 입력하지 않았습니다');
             errorFound = true;
         }
@@ -82,44 +92,113 @@ function checkInputErr(arr, rightNumOfArgs) {
     return errorFound;
 }
 
+const inputErr = {
+    withOneArgs(argList) {
+        const rightNumOfArgs = 1;
+        return checkInputErr(argList,rightNumOfArgs);
+    },
+    withTwoArgs(argList) {
+        const rightNumOfArgs = 2;
+        return checkInputErr(argList,rightNumOfArgs);
+    },
+    withThreeArgs(argList) {
+        const rightNumOfArgs = 3;
+        return checkInputErr(argList,rightNumOfArgs);
+    }
+};
+
 const area = {
     circle(circleRadius) {
         const circleArea = computeArea.circle(circleRadius);
-        const inputArr = Array.from(arguments);
-        if(checkInputErr(inputArr, 1)) {return false;}
+        const argList = Array.from(arguments);
+        if(inputErr.withOneArgs(argList)) {return false;}
     
         printArea.circle(circleRadius, circleArea);
     },
-    square(squareWidth, squareHeight) {
-        const squareArea = computeArea.square(squareWidth, squareHeight);
-        const inputArr = Array.from(arguments);
-        if(checkInputErr(inputArr, 2)) {return false;}
+    rect(rectWidth, rectHeight) {
+        const rectArea = computeArea.rect(rectWidth, rectHeight);
+        const argList = Array.from(arguments);
+        if(inputErr.withTwoArgs(argList)) {return false;}
     
-        printArea.square(squareWidth, squareHeight, squareArea);
+        printArea.rect(rectWidth, rectHeight, rectArea);
     },
     trape(trapeTop, trapeBottom, trapeHeight) {
         const trapeArea = computeArea.trapezoid(trapeTop, trapeBottom, trapeHeight);
-        const inputArr = Array.from(arguments);
-        if(checkInputErr(inputArr, 3)) {return false;}
+        const argList = Array.from(arguments);
+        if(inputErr.withThreeArgs(argList)) {return false;}
     
         printArea.trapezoid(trapeTop, trapeBottom, trapeHeight, trapeArea);
     },
     cylin(cylinRadius, cylinHeight) {
         const cylinArea = computeArea.cylinder(cylinRadius, cylinHeight);
-        const inputArr = Array.from(arguments);
-        if(checkInputErr(inputArr, 2)) {return false;}
+        const argList = Array.from(arguments);
+        if(inputErr.withTwoArgs(argList)) {return false;}
     
         printArea.cylinder(cylinRadius, cylinHeight, cylinArea);
     }
 };
 
+function getArea(...arguments) {
+    let calcType = arguments[0];
+    const argList = arguments.splice(1);
+    if(calcType === 'circle' && argList.length >= 2) {calcType = 'growingCircle';}
+
+    switch (calcType) {
+        case 'circle':
+            if(inputErr.withOneArgs(argList)) {return false;}
+            const circleRadius = argList[0];
+            const circleArea = computeArea.circle(circleRadius);
+            printArea.circle(circleRadius, circleArea);    
+            break;
+
+        case 'growingCircle':
+            if(inputErr.withTwoArgs(argList)) {return false;}
+            let startRadius = argList[0], targetRadius = argList[1];
+            if(startRadius > targetRadius){
+                console.log('시작 반지름 넓이가 최종 반지름보다 작습니다!');
+                return false;
+            }
+            for (let radius = startRadius; radius <= targetRadius; radius++) {
+                const circleArea = computeArea.circle(radius);
+                printArea.circle(radius, circleArea);
+            }
+            break;
+            
+        case 'rect':
+            if(inputErr.withTwoArgs(argList)) {return false;}
+            const rectWidth = argList[0], rectHeight = argList[1];
+            const rectArea = computeArea.rect(rectWidth, rectHeight);
+            printArea.rect(rectWidth, rectHeight, rectArea);
+            break;
+
+        case 'trapezoid':
+            if(inputErr.withThreeArgs(argList)) {return false;}
+            const trapeTop = argList[0], trapeBottom = argList[1], trapeHeight = argList[2];
+            const trapeArea = computeArea.trapezoid(trapeTop, trapeBottom, trapeHeight);
+            printArea.trapezoid(trapeTop, trapeBottom, trapeHeight, trapeArea);
+            break;
+
+        case 'cylinder':
+            if(inputErr.withTwoArgs(argList)) {return false;}
+            const cylinRadius = argList[0], cylinHeight = argList[1];
+            const cylinArea = computeArea.cylinder(cylinRadius, cylinHeight);
+            printArea.cylinder(cylinRadius, cylinHeight, cylinArea);
+            break;
+
+
+        default:
+            break;
+    }
+
+
 function greeting() {
     console.log(
         `다각형 넓이 계산기입니다. 아래 예시를 보고 명령어를 입력해 주세요
-        - 원: area.circle(반지름 숫자) [예: area.circle(2)]
-        - 사각형: area.square(너비, 높이) [예: area.square(2,3)]
-        - 사다리꼴: area.trape(윗변 길이, 아랫변 길이, 높이) [예: area.trape(2,3,5)]
-        - 원기둥: area.cylin(반지름, 높이) [예: area.cylin(2,5)]
+        - 원 넓이: getArea('circle', 반지름 숫자) [예: getArea('circle', 2)]
+        - 점점 커지는 원 넓이: getArea('circle', 시작 반지름 숫자, 최대 반지름 숫자) [예: getArea('circle', 2, 5)]
+        - 사각형: getArea('rect', 너비, 높이) [예: getArea('rect', 2, 3)]
+        - 사다리꼴: getArea('trapezoid', 윗변 길이, 아랫변 길이, 높이) [예: area('trapezoid', 2,3,5)]
+        - 원기둥: getArea('cylinder', 반지름, 높이) [예: getArea('cylinder', 2,5)]
         `
     );
 }
@@ -127,12 +206,17 @@ function greeting() {
 greeting();
 
 
+
 //test cases
-console.log(`circleArea(1)`);
-circleArea(1);
-console.log(`circleArea()`);
-circleArea();
-console.log(`circleArea('a')`);
-circleArea('a');
-console.log(`circleArea('a', 2, 3)`);
-circleArea('a', 2, 3);
+getArea('circle', 2);
+getArea('circle', 2,5);
+getArea('circle', 5,2);
+getArea('rect', 2,5);
+getArea('rect', 2,5,7);
+getArea('rect', 2,5,'a');
+getArea('trapezoid', 2,5,7);
+getArea('trapezoid', 2,5,'a');
+getArea('trapezoid', 2,7);
+getArea('cylinder', 2,5,7);
+getArea('cylinder', 2,5);
+getArea('cylinder', 2);
