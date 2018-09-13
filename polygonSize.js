34304e40f7c4e4c0a62f6cf0ab337fafd3f46791
Head <mrdoo309@gmail.com>
2018-09-13 17:04:37 +0900

    modify by referring to feedback, add function getArea, add function printExecutionSequence

--- polygonSize.js
+++ polygonSize.js
@@ -1,67 +1,80 @@
-function sizeCircle(radius) {
-    if (toString.call(radius) != '[object Number]') {
+var arr = [];
+
+function isNum(length) {
+    if (toString.call(length) != '[object Number]') {
+        return true
+    }
+    return false
+}
+
+function sizeCircle(radius1, radius2) {
+    if (arguments.length === 2) {
+        let sum = 0;
+        for (let i = radius1; i <= radius2; i++) {
+            sum = sum + (i * i * Math.PI)
+            arr.push('circle');
+        }
+        console.log('원 모두의 합은 ' + sum + '입니다.');
+        return;
+    } else if (arguments.length != 1) {
+        return console.log('인자의 개수가 맞지 않습니다.');
+    } else if (isNum(radius1)) {
         return console.log('숫자만 계산 가능합니다.');
     }
-    const circleArea = radius * radius * Math.PI
-    console.log('반지름이 ' + radius + '인 원의넓이는' + circleArea + '입니다.');
+    const circleArea = radius1 * radius1 * Math.PI
+    console.log('반지름이 ' + radius1 + '인 원의넓이는' + circleArea + '입니다.');    
+    arr.push('circle');
 }
-//test circle
-console.log('원')
-sizeCircle(4)
-sizeCircle('문자')
-sizeCircle();
 
 function sizeSquare(lowerSide, height) {
-    if (lowerSide === undefined || height === undefined) {
-        return console.log('인자가 부족합니다.')
-    } else if (toString.call(lowerSide) != '[object Number]' || toString.call(height) != '[object Number]') {
-        return console.log('숫자만 계산 가능합니다.')
+    if (arguments.length != 2) {
+        return console.log('인자의 개수가 맞지 않습니다.');
+    } else if (isNum(lowerSide) || isNum(height)) {
+        return console.log('숫자만 계산 가능합니다.');
     }
     const squareArea = lowerSide * height
-    console.log('밑변과 높이가 ' + lowerSide + ', ' + height + '인 사각형의 넓이는' + squareArea + '입니다.')
+    console.log('밑변과 높이가 ' + lowerSide + ', ' + height + '인 사각형의 넓이는' + squareArea + '입니다.');
+    arr.push('rect');
 }
-
-//test square
-console.log('사각형')
-sizeSquare(4, 3)
-sizeSquare(3)
-sizeSquare()
-sizeSquare('문자', '문자');
-sizeSquare('문자');
 
 function sizeTrapezoid(topSide, lowerSide, height) {
-    if (topSide === undefined || lowerSide === undefined || height === undefined) {
-        return console.log('인자가 부족합니다.')
-    } else if (toString.call(topSide) != '[object Number]' || toString.call(lowerSide) != '[object Number]' || toString.call(height) != '[object Number]') {
-        return console.log('숫자만 계산할 수 있습니다.')
+    if (arguments.length != 3) {
+        return console.log('인자의 개수가 맞지 않습니다.');
+    } else if (isNum(topSide) || isNum(lowerSide) || isNum(height)) {
+        return console.log('숫자만 계산할 수 있습니다.');
     }
     const trapezoidArea = (topSide + lowerSide) * height / 2
-    console.log('윗변과 아랫변과 높이가 ' + topSide + ', ' + lowerSide + ', ' + height + '인 사다리꼴의 넓이는' + trapezoidArea + '입니다.')
+    console.log('윗변과 아랫변과 높이가 ' + topSide + ', ' + lowerSide + ', ' + height + '인 사다리꼴의 넓이는' + trapezoidArea + '입니다.');
+    arr.push('trapezoid');
 }
-
-//test trapezoid
-console.log('사다리꼴')
-sizeTrapezoid(2, 3, 4)
-sizeTrapezoid(2, 3)
-sizeTrapezoid('문자', 2, 3)
-sizeTrapezoid('문자', '문자', '문자')
-sizeTrapezoid()
 
 function sizeSylinder(radius, height) {
-    if (radius === undefined || height === undefined) {
-        return console.log('인자가 부족합니다.')
-    } else if (toString.call(radius) != '[object Number]' || toString.call(height) != '[object Number]') {
-        return console.log('숫자만 계산할 수 있습니다.')
+    if (arguments.length != 2) {
+        return console.log('인자의 갯수가 맞지 않습니다.');
+    } else if (isNum(radius) || isNum(height)) {
+        return console.log('숫자만 계산할 수 있습니다.');
     }
-    const sylinderArea =  (Math.PI * radius * radius) + ((2 * radius * Math.PI) * height)
-    console.log('반지름과 높이가' + radius + ', ' + height + '인 원기둥의 겉넓이는 ' + sylinderArea + '입니다.')
+    const sylinderArea = 2 * (Math.PI * radius * radius) + (2 * (radius * Math.PI) * height)
+    console.log('반지름과 높이가' + radius + ', ' + height + '인 원기둥의 겉넓이는 ' + sylinderArea + '입니다.');
+    arr.push('sylinder')
 }
 
-//test sizeSylinder
-console.log('원기둥')
-sizeSylinder(2, 3)
-sizeSylinder(2)
-sizeSylinder('문자')
-sizeSylinder('문자', 3)
-sizeSylinder()
+function getArea(polygon, num1, num2, num3) {
+    if (polygon === 'circle' && arguments.length === 3) {
+        sizeCircle(num1, num2)
+    } else if (polygon === 'rect') {
+        sizeSquare(num1, num2)
+    } else if (polygon === 'trapezoid') {
+        sizeTrapezoid(num1, num2, num3)
+    } else if (polygon === 'Sylinder') {
+        sizeSylinder(num1, num2)
+    } else if (polygon === 'circle') {
+        sizeCircle(num1)
+    }
+}
 
+function printExecutionSequence() {
+    arr.forEach(function(v, i) {
+        console.log((i + 1) + '번째 : ' + v)
+    })
+}
