6b42d10cd521beff2e0e1016f8506f0e6ff3200a
Head <mrdoo309@gmail.com>
2018-09-14 15:40:16 +0900

    modifiy function sizeCircle to CalcCircleWidth and change inner contents

--- polygonSize.js
+++ polygonSize.js
@@ -25,24 +25,26 @@ function isTypeNumber(number) {
 
 //2. 인자를 구분할 때 !== 를 최대한 안쓰고 단순화시키는 걸로 개선
 //3. 계산하는 함수들의 이름과 인자들의 명칭들 모두 변경
-function sizeCircle(radius1, radius2) {
-    if (arguments.length === 2) {
-        let sum = 0;
-        for (let i = radius1; i <= radius2; i++) {
-            sum = sum + (i * i * Math.PI)
-            arr.push('circle');
-        }
-        console.log('원 모두의 합은 ' + sum + '입니다.');
+function CalcCircleWidth(radius1, radius2) {
+    if (radius1 > radius2) {
+        console.log(radius1 + '부터' + radius2 + '까지라구요? 다시 한번 생각해주세요')
         return;
-    } else if (arguments.length != 1) {
-        return console.log('인자의 개수가 맞지 않습니다.');
-    } else if (isNum(radius1)) {
-        return console.log('숫자만 계산 가능합니다.');
     }
-    const circleArea = radius1 * radius1 * Math.PI
-    console.log('반지름이 ' + radius1 + '인 원의넓이는' + circleArea + '입니다.');    
-    arr.push('circle');
+    if (isNum(...arguments) && arguments.length === 1) {
+        const circleWidth = radius1 * radius1 * Math.PI
+        console.log('반지름이 ' + radius1 + '인 원의넓이는' + circleWidth + '입니다.');
+        ExecutionSequenceArray.push('circle');
+        return;
+    } else if (isNum(...arguments) && arguments.length === 2) {
+        for (let i = radius1; i <= radius2; i++) {
+            console.log('반지름이' + i + '인 원의 넓이 : ' + (i * i * Math.PI));
+            ExecutionSequenceArray.push('circle')
+        }
+        return;
+    }
+    console.log('인자의 갯수가 맞지 않거나 숫자가 아닙니다.');
 }
+
 
 function sizeSquare(lowerSide, height) {
     if (arguments.length != 2) {
