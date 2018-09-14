b5ccc1246dc2d5cdcb541cab60d8ae9341baec8d
sk2504 <sk2504@naver.com>
2018-09-14 11:03:42 +0900

    Step1 다각형 넓이 구하기 (#28)
    * step1 다각형 넓이 구하기 커밋입니다.
    
    * Step2 1-1 요구사항 수정
    
    * Step2 1-2 요구사항
    
    printExecutionSequence 함수를 만들어 로깅을 구현,
    실행 횟수기록 기능도 추가하였습니다
    
    * step2 요구사항 수정
    
    요구사항에 맞게 수정해보았습니다 피드백 부탁드립니다!
    에러체크 로직을 최대한 밖으로 빼서 작동되게 노력해봤습니다
    
    * step2-1 수정사항 입니다
    
    파라미터 정의와 몇몇 함수이름 변경을 하였으며,대소문자 구분을 했습니다
    도저히 생각이 않나서 다른분들껄 참고해서 코드를 짯는데
    다음부턴 혼자힘으로 최대한 하겠습니다!!

--- index.js
+++ index.js
@@ -1,50 +1,135 @@
-function calculateCircleSize(radius1,radius2) {
-    var size = 0;
-    if(arguments.length === 1){
-        size = radius1 * radius1 * Math.PI;
-        console.log("원의 넓이: " + size);
+// printExecutionSequence 함수 만들기
+let logFunctions = [];
+let functionCount = 0;
+function printExecutionSequence() {
+    console.log('계산 실행순서 : ' + logFunctions.join());
+    console.log('계산 실행 횟수 : ' + functionCount);
+}
+function addExecutionlog(shape) {
+    logFunctions.push(shape);
+    functionCount++;
+}
+// 모든에러체크 동작 함수
+function checkError(...arguments) {
+    if (isUndefined(...arguments)) {
+        console.log('값을 입력하세요.')
+        return true;
+    } else if (isPositiveNumber(...arguments)) {
+        console.log('0보다 큰수만 입력하세요.')
+        return true;
+    } else if (isNumber(...arguments)) {
+        console.log('숫자만 입력해주세요.')
+        return true;
     }
-    if(arguments.length === 2){
-        for(var i = radius1; i <= radius2; i++){
-            console.log('반지름이 '+i+'인 원의 넓이는 :'+(i*i*Math.PI));
+}
+// 숫자 확인 함수
+function isNumber(...arguments) {
+    let len = arguments.length;
+    for (let i = 0; i < len; i++)
+        if (typeof arguments[i] !== 'number') {
+            return true;
+        }
+    return false;
+}
+// 양수 확인 함수
+function isPositiveNumber(...arguments) {
+    let len = arguments.length;
+    for (let i = 0; i < len; i++) {
+        if (arguments[i] <= 0) {
+            return true;
+        }
+    }
+    return false;
+}
+// 인자값이 있는지 확인하는 함수
+function isUndefined(...arguments) {
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === 'undefined') {
+            return true;
+        }
+        return false;
+    }
+}
+// 1. 반지름을 입력받아 원의 넓이 계산
+function circleSizecalc(radius1) {
+    if (checkError(radius1)) {
+        return;
+    } else {
+        let result = Math.PI * Math.pow(radius1, 2);
+        console.log('원의 넓이는' + result + '입니다');
+        addExecutionlog('circle')
+    }
+}
+// 1-1. 반지름을 입력받아 radius2값 만큼 계산
+function circlesSizecalc(radius1, radius2) {
+    if (checkError(radius1, radius2)) {
+        return;
+    } else {
+        for (i = radius1; i <= radius2; i++) {
+            let result = Math.PI * Math.pow(i, 2);
+            console.log('반지름' + i + '인 원의 넓이는' + result + '입니다');
+            addExecutionlog('circle');
         }
     }
 }
-
-function calculateRectSize(width,height) {
-    if(typeof width !== 'number' && typeof height !== 'number'){
-        console.log('숫자형타입만 계산이 가능합니다');
-    } else if(arguments.length===0){
-        console.log('최소 한가지 값이 필요합니다')
-    } else if(width<= 0 || height<= 0){
-        console.log('길이값과 높이값은 0보다 커야합니다');
-    }
-    else {
-        var size = width * height / 2;
-        console.log("사각형의 넓이: " + size);
+// 2. 사각형의 넓이 계산
+function rectCalc(base, height) {
+    if (checkError(base, height)) {
+        return;
+    } else {
+        let result = base * height;
+        console.log('사각형의 넓이는' + result + '입니다');
+        addExecutionlog('rect');
     }
 }
-
-function calculateTrapeSize(upper,bottom,height) {
-    if(arguments.length<=2){
-        console.log('3개의 인자가 필요합니다');
+// 3.사다리꼴의 넓이
+function trapezoidSizecalc(base1, base2, height) {
+    if (checkError(base1, base2, height)) {
+        return;
+    } else {
+        let result = (base1 + base2) / 2 * height;
+        console.log('사다리꼴의 넓이는' + result + '입니다');
+        addExecutionlog('trapezoid');
     }
-
-    var size = upper*bottom*height/2;
-    console.log("사다리꼴의 넓이: "+size);
 }
-
-
-
+// 4. 원기둥의 겉넓이 계산
+function cylinderSizecalc(radius, height) {
+    if (checkError(radius, height)) {
+        return;
+    } else {
+        let result = 2 * Math.PI * radius * (radius + height)
+        console.log('원기둥의 겉넓이는 ' + result + '입니다');
+        addExecutionlog('cylinder');
+    }
+}
+// getArea 함수를 사용하면 모든도형의 넓이값을 얻는다.
 function getArea(shape, para1, para2, para3) {
-    switch(shape){
-        case 'circle': calculateCircleSize(para1, para2);
+    switch (shape) {
+        case 'circle': circleSizecalc(para1)
             break;
-        case 'rect': calculateRectSize(para1, para2);
+        case 'circleAll': circlesSizecalc(para1, para2)
             break;
-        case 'trape': calculateTrapeSize(para1, para2, para3);
+        case 'rect': rectCalc(para1, para2)
+            break;
+        case 'trapezoid': trapezoidSizecalc(para1, para2, para3)
+            break;
+        case 'Cylinder': cylinderSizecalc(para1, para2)
             break;
     }
 }
 
-getArea('circle',1,10);
+getArea('circle')
+getArea('circle', 5)
+
+getArea('circleAll', 5, 'a');
+getArea('circleAll', 5, 8)
+
+getArea('rect', 5, -10);
+getArea('rect', 5, 10);
+
+getArea('trapezoid', 5, 5);
+getArea('tripezoid', 5, 5, 10);
+
+getArea('Cylinder', 5, 0);
+getArea('Cylinder', 5, 10);
+printExecutionSequence();
