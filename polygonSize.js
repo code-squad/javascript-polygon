f21a7b48ff588e41a44a6c1f5a05ea7979cf4f92
Head <mrdoo309@gmail.com>
2018-09-14 17:35:49 +0900

    Revise little bugs,(add return to calcSylinderWidth, change some upperCase to lowerCase

--- polygonSize.js
+++ polygonSize.js
@@ -1,9 +1,9 @@
 //변수 const나 let으로 변경, 변수이름 개선
-const ExecutionSequenceArray = [];
+const executionSequenceArray = [];
 
 //arrow함수 이용
 function printExecutionSequence() {
-    ExecutionSequenceArray.forEach((v, i) => {
+    executionSequenceArray.forEach((v, i) => {
         console.log((i + 1) + '번째 : ' + v)
     })
 }
@@ -33,12 +33,12 @@ function calcCircleWidth(radius1, radius2) {
     if (isNum(...arguments) && arguments.length === 1) {
         const circleWidth = radius1 * radius1 * Math.PI
         console.log('반지름이 ' + radius1 + '인 원의넓이는' + circleWidth + '입니다.');
-        ExecutionSequenceArray.push('circle');
+        executionSequenceArray.push('circle');
         return;
     } else if (isNum(...arguments) && arguments.length === 2) {
         for (let i = radius1; i <= radius2; i++) {
             console.log('반지름이' + i + '인 원의 넓이 : ' + (i * i * Math.PI));
-            ExecutionSequenceArray.push('circle')
+            executionSequenceArray.push('circle')
         }
         return;
     }
@@ -50,7 +50,7 @@ function calcRectWidth(RectLowerSide, RectHeight) {
     if(isNum(...arguments) && arguments.length === 2) {
         const RectWidth = RectLowerSide * RectHeight
         console.log('밑변이' + RectLowerSide + '높이가' + RectHeight + '인 사각형의 넓이는' + RectWidth + '입니다.');
-        ExecutionSequenceArray.push('Rect')
+        executionSequenceArray.push('Rect')
         return;
     }
     console.log('인자의 갯수가 맞지 않거나 숫자가 아닙니다.')
@@ -60,7 +60,7 @@ function calcTrapezoidWidth(trapezoidLowerSide, trapezoidTopSide, trapezoidHeigh
     if(isNum(...arguments) && arguments.length === 3) {
         const trapezoidWidth = (trapezoidLowerSide + trapezoidTopSide) * trapezoidHeight / 2
         console.log('밑변이 ' + trapezoidLowerSide + '아랫변이 ' + trapezoidLowerSide + '높이가 ' + trapezoidHeight + '인 사다리꼴의 넓이는 ' + trapezoidWidth + '입니다.')
-        ExecutionSequenceArray.push('trapezoid');
+        executionSequenceArray.push('trapezoid');
         return;
     }
     console.log('인자의 갯수가 맞지 않거나 숫자가 아닙니다.')
@@ -70,11 +70,14 @@ function calcSylinderWidth (sylinderRadius, sylinderHeight) {
     if(isNum(...arguments) && arguments.length === 2) {
         const sylinderWidth = 2 * (sylinderRadius * sylinderRadius * Math.PI) + ((2 * Math.PI * sylinderRadius) * sylinderHeight)
         console.log('반지름이 ' + sylinderRadius + '높이가 ' + sylinderHeight + '인 원기둥의 넓이는 ' + sylinderWidth + '입니다.');
-        ExecutionSequenceArray.push('sylinder');
+        executionSequenceArray.push('sylinder');
+        return;
     }
     console.log('인자의 갯수가 맞지 않거나 숫자가 아닙니다.');
 }
 
+
+//4. 함수 인자의 갯수 확인
 function getArea(polygon, num1, num2, num3) {
     if (polygon === 'circle' && arguments.length === 3) {
         calcCircleWidth(num1, num2)
@@ -85,7 +88,7 @@ function getArea(polygon, num1, num2, num3) {
     } else if (polygon === 'trapezoid' && arguments.length === 4) {
         calcTrapezoidWidth(num1, num2, num3)
         return;
-    } else if (polygon === 'Sylinder' && arguments.length === 3) {
+    } else if (polygon === 'sylinder' && arguments.length === 3) {
         calcSylinderWidth(num1, num2)
         return;
     } else if (polygon === 'circle' && arguments.length === 2) {
@@ -94,3 +97,9 @@ function getArea(polygon, num1, num2, num3) {
     }
     console.log('인자의 갯수가 맞지 않거나 숫자가 아닙니다.')
 }
+
+getArea('circle', 2, 3);
+getArea('rect', 2, 3);
+getArea('trapezoid', 2, 3, 4);
+getArea('sylinder', 2, 3);
+getArea('meiv', 3, '2');
