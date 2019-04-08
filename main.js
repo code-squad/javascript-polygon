c802c4ef278997e0ab7d1da545599759f0532d8a
cocahack <cocahack@naver.com>
2019-04-08 22:30:35 +0900

    Revise the source code Use pow function Use every function for validating arguments Use try-catch statement Seperate getArea function from readline.question

--- main.js
+++ main.js
@@ -1,47 +1,81 @@
 const readline = require('readline');
+const { pow, PI } = Math;
+const helpMsg = '넓이를 구하는데 필요한 정보를 입력해주세요.\n' 
+               +'원 넓이: circle radius\n'
+               +'사각형 넓이: rectangle width height\n'
+               +'사다리꼴 넓이: trapezoid line1 line2 height\n'
+               +'원기둥 넓이: cylinder radius height\n';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('도형의 유형과 넓이를 구하는데 필요한 정보를 입력해주세요.', (info) => {
-  const args = info.split(' ');
-  const shapeType = args[0];
-  const rest = args.slice(1).map(e => parseInt(e));
-  let result = 0;
-  if(checkNumber(rest) === false) throw new Error('숫자가 필요합니다.');
-
-  if(checkArgsLength(rest.length, shapeArea[shapeType].length) === false)
-    throw new Error('필요한 인자의 개수가 부족합니다');
-  result = shapeArea[shapeType](rest);
-
-  console.log(result);
-
-  rl.close();
-});
-
 const shapeArea = {
-  circle: ([radius]) => radius * radius * Math.PI,
-  rectangle: ([a, b]) => a * b,
-  trapezoid: ([a, b, height]) => 2 * height * (a + b),
+  circle: ([radius]) => pow(radius, 2) * PI,
+  rectangle: ([width, height]) => width * height,
+  trapezoid: ([line1, line2, height]) => 2 * height * (line1 + line2),
   cylinder: function ([radius, height]) {
     return 2 * this.circle([radius]) + circumference([radius]) * height;
   }
 }
 
-function circumference([radius]){
-  return 2 * radius * Math.PI;
+const parameterLength = {
+  circle: 1,
+  rectangle: 2,
+  trapezoid: 3,
+  cylinder: 2,
 }
 
-function checkNumber(target){
-  let ret = true;
-  target.forEach(element => {
-    if(typeof element !== 'number') ret = false;
-  });
-  return ret;
+function circumference([radius]){
+  return 2 * radius * PI;
+}
+
+function isNumber(element){
+  return typeof element === 'number';
+}
+
+function isNotNaN(element){
+  return !isNaN(element);
+}
+
+function checkNumber(args){
+  return args.every(isNumber) && args.every(isNotNaN);
 }
 
 function checkArgsLength(argsLen, funcLen){
   return argsLen >= funcLen;
 }
+
+function getArea(type, args){
+  try {
+    if(shapeArea[type] === undefined){
+      throw new Error('circle, rectangle, trapezoid, cylinder 이외에는 사용할 수 없습니다.');
+    }
+
+    if(!checkNumber(args)) {
+      throw new Error('radius, line, height는 숫자여야 합니다.');
+    }
+
+    if(!checkArgsLength(args.length, parameterLength[type].length)){
+      throw new Error('필요한 인자의 개수가 부족합니다');
+    }
+
+    return shapeArea[type](args);
+  } catch (error) {
+    console.error(`에러: ${error.message}`);
+  }
+}
+
+
+rl.question(helpMsg, (info) => {
+  const args = info.split(' ');
+  const shapeType = args[0];
+  const rest = args.slice(1).map(e => parseInt(e));
+  
+  const result = getArea(shapeType, rest);
+
+  console.log(result || '');
+
+  rl.close();
+});
