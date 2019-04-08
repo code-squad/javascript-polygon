5b35b4bff2f5a0267fe5bf3a000ed3b925bbcaf3
Soojin Roh <sooreal91@gmail.com>
2019-04-08 17:43:06 +0900

    step2_1 다각형의 넓이 구현
    1. 반지름을 입력받아 원의 넓이를 계산하는 함수
    2. 사각형의 넓이를 계산하는 함수
    3. 사다리꼴의 넓이를 계산하는 함수
    4. 원기둥의 넓이를 계산하는 함수
    위의 네 개의 함수를 구현하고 숫자가 아닌 경우, 인자의 갯수가 틀린 경우 에러를 반환합니다.

---
+++ STEP2_1.js
@@ -0,0 +1,48 @@
+const {PI} = Math
+
+function checkType(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof(arr[i]) !== "number") {
+            throw "please input Number data type";
+        }
+    }
+}
+
+function checkArgumentCount(arr, correctCount) {
+    if (arr.length !== correctCount) {
+        throw "please input correct number of arguments"
+    }
+}
+
+function circleArea(r) {
+    checkArgumentCount(arguments, 1);
+    checkType(arguments);
+
+    return PI * Math.pow(r, 2)
+}
+
+function squareArea(width, height) {
+    checkArgumentCount(arguments, 2);
+    checkType(arguments);
+
+    return width * height;
+}
+
+function trapezoidArea(x1, x2, h) {
+    checkArgumentCount(arguments, 3);
+    checkType(arguments);
+
+    return (x1 + x2) * h / 2
+}
+
+function cylinderArea(r, h) {
+    checkArgumentCount(arguments, 2);
+    checkType(arguments);
+
+    return (2 * PI * Math.pow(r, 2)) + 2 * PI * r * h
+}
+
+console.log(circleArea(3));
+console.log(squareArea(2, 7));
+console.log(trapezoidArea(6, 3, 3));
+console.log(cylinderArea(4, 6));
