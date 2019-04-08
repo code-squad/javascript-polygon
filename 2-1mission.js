07fb2439458a2324a96b81c6e55bc897850ae886
Min-92 <okssud1@naver.com>
2019-04-08 21:01:16 +0900

    다각형 넓이 구하기 미션 개선
    인자체크 하위함수로 추가, != -> !== 변경

--- 2-1mission.js
+++ 2-1mission.js
@@ -1,53 +1,56 @@
 
-function areaC(r) {
-    let area = 0;
-    if(arguments.length != 1){
-        throw "인자 갯수가 맞지않습니다.";
-    }
-    if(typeof r != "number"){
+function typeNumber(a){
+    if(typeof a !== "number"){
         throw "숫자를 입력하세요";
     }
+}
 
+function areaOfCircle(r) {
+    let area = 0;
+    if(arguments.length !== 1){
+        throw "인자 갯수가 맞지않습니다.";
+    }
+    for(let i in arguments){
+        typeNumber(arguments[i]);
+    }
     area = Math.PI*r*r;
-
     return area.toFixed(2);
 }
 
-function areaR(a,b){
+function areaOfRectangular(a,b){
     let area = 0;
-    if(arguments.length != 2){
+    if(arguments.length !== 2){
         throw "인자 갯수가 맞지않습니다.";
     }
-    if(typeof a != "number" || typeof b != "number"){
-        throw "숫자를 입력하세요";
+    for(let i in arguments){
+        typeNumber(arguments[i]);
     }
     area = a*b;
+    return area;
+}
+
+function areaOfTrapezoid(a,b,h){
+    let area = 0;
+    if(arguments.length !== 2){
+        throw "인자 갯수가 맞지않습니다.";
+    }
+    for(let i in arguments){
+        typeNumber(arguments[i]);
+    }
+    area = (a+b)*h/2;
 
     return area;
 }
 
-function areaT(a,h){
+function areaOfCylinder(r,h){
     let area = 0;
-    if(arguments.length != 2){
+    if(arguments.length !== 2){
         throw "인자 갯수가 맞지않습니다.";
     }
-    if(typeof a != "number" || typeof h != "number"){
-        throw "숫자를 입력하세요";
+    for(let i in arguments){
+        typeNumber(arguments[i]);
     }
-    area = a*h;
-
-    return area;
-}
-
-function areaS(r,h){
-    let area = 0;
-    if(arguments.length != 2){
-        throw "인자 갯수가 맞지않습니다.";
-    }
-    if(typeof r != "number" || typeof h != "number"){
-        throw "숫자를 입력하세요";
-    }
-    let AC = areaC(r);
+    let AC = areaOfCircle(r);
     area = 2*AC+2*Math.PI*r*h;
     
     return area.toFixed(2);
