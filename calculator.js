88afa2f0a571f2ca04956433fdfde757761e4257
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-08 17:14:55 +0900

    자바스크립트 step 2-1 : 다각형 넓이 계산기 구현
    TUNA, YM 이 함께 페어프로그래밍

--- calculator.js
+++ calculator.js
@@ -1,52 +1,52 @@
 let getCircleArea = function(r) {
-    checkError(r);
-    checkPramNum(1, arguments.length);
+    checkIsNumError(arguments);
+    checkParamNum(r);
     return Math.PI * r * r;
 }
 
 let getSquareArea = function(width, height) {
-    checkError(height);
-    checkPramNum(2, arguments.length);
+    checkIsNumError(arguments);
+    checkParamNum(width, height);
     return width * height;
 }
 
 let getTrapezoidArea = function(top, bottom, height) {
-    checkError(height);
-    checkPramNum(3, arguments.length);
+    checkIsNumError(arguments);
+    checkParamNum(top, bottom, height);
     return ((top + bottom) * height) / 2
 }
 
 let getCylinderArea = function(r, height) {
-    checkError(height);
-    checkPramNum(2, arguments.length);
+    checkIsNumError(arguments);
+    checkParamNum(r, height);
     return getCircleArea(r) * height;
 }
 
-let checkError = function(...obj) {
-    for (let val of obj) {
+let checkIsNumError = function(args) {
+    for (let val of args) {
         if (typeof val !== "number") {
             throw new Error("인자값이 숫자가 아닙니다");
         }
     }
 }
 
-let checkPramNum = function(N, length) {
-    if (length < N) {
-        throw new Error("인자의 갯수가 부족합니다.");
+let checkParamNum = function(...args) {
+    for (let val of args) {
+        if (typeof val === "undefined") {
+            throw new Error("인자의 갯수가 부족합니다.");
+        }
     }
 }
 
-//console.log(getCircleArea(5));
-//console.log(getCircleArea("asdf"));
-//console.log(getCircleArea());
+// console.log(getCircleArea(5));
 
-//console.log(getSquareArea(2, 4));
-//console.log(getSquareArea("안녕난문자"));
-//console.log(getSquareArea(1));
+// console.log(getSquareArea("ss", 2));
+// console.log(getSquareArea("안녕난문자"));
+// console.log(getSquareArea(1));
 
-//console.log(getTrapezoidArea(4, 6, 5));
-//console.log(getTrapezoidArea(true));
-//console.log(getTrapezoidArea(2, 4));
+// console.log(getTrapezoidArea(4, "asdf", 5));
+// console.log(getTrapezoidArea(true));
+// console.log(getTrapezoidArea(null));
 
 //console.log(getCylinderArea(5, 10));
 //console.log(getCylinderArea(undefined));
