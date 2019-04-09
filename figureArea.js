842249b150887b77865535c8010c7cb71b5d98a9
seldev <35516239+sel-dev@users.noreply.github.com>
2019-04-09 12:35:09 +0900

    Create step2 (#65)
    * Create - step2 figureArea.js
    
    -반지름을 입력받아 원의 넓이를 계산하는 함수를 만든다.
    -필요한 인자를 입력받아 사각형의 넓이를 계산하는 함수를 만든다.
    -필요한 인자를 입력받아 사다리꼴의 넓이를 계산하는 함수를 만든다.
    -필요한 인자를 입력받아 원기둥의 넒이를 계산하는 함수를 만든다.
    -숫자가 아니면 에러를 반환하도록 구현한다.
    -인자의갯수가 부족하면 에러를 반환한다.
    
    * Refator - checkParamCount, circleArea

---
+++ figureArea.js
@@ -0,0 +1,47 @@
+function isNum(param){
+    if(param.length === 0) {
+        throw Error("you don't input any argument!!")
+    }else{
+        param.map(function(v){
+            if(typeof v !== "number") {
+                throw Error("plz input Number type")
+            }
+        })
+    }
+  
+    return true
+}
+
+function checkParamCount(count, param) {
+        if(!(count === param.length)){
+            throw Error(`input ${count} argument, your argument count is ${param.length}`)
+        }
+        return true
+        
+    }   
+function checkParam(count, param){
+    return isNum(param) && checkParamCount(count, param) && [ ... param]
+}
+
+function circleArea(...param){
+    const [r] = checkParam(1, param);
+    return 3.14 * r * r 
+}
+
+function recArea(...param) {
+    const [w, h] = checkParam(2,param); 
+    return w * h;
+}
+
+function trepArea(...param) {
+    const [t, b, h] = checkParam(3,param);
+    return (t+b)/2 * h;
+}
+
+function pillar(...param) {
+    const [r, h] = checkParam(2, param);
+    return circleArea(r) * h
+}
+
+console.log(circleArea(5))
+console.log(recArea(0,4,5))
