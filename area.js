d2dc1c7f38f7633ac8558cacb69e84a91f16bb9e
Hoyoung Jung <honux77@gmail.com>
2019-04-09 14:26:41 +0900

    Remove exports for validArgs
    export에 불필요하므로 제거함
    더불어 테스트 코드에서도 제거

--- area.js
+++ area.js
@@ -2,8 +2,6 @@
 [ ]필요한 인자를 입력받아 사각형의 넓이를 계산하는 함수를 만든다.
 [ ]필요한 인자를 입력받아 사다리꼴의 넓이를 계산하는 함수를 만든다.
 [ ]필요한 인자를 입력받아 원기둥의 넒이를 계산하는 함수를 만든다.
-[ ]숫자가 아니면 에러를 반환하도록 구현한다.
-[ ]인자의갯수가 부족하면 에러를 반환한다.
 */
 
 /*
@@ -27,6 +25,5 @@ function areaOfCircle(radius) {
 }
 
 module.exports = {
-    validArgs, //TODO: remove it before next step
     areaOfCircle,
 };
--- test.js
+++ test.js
@@ -1,18 +1,5 @@
 const area = require('./area');
 
-//test valid args
-(function (){    
-    console.log(area.validArgs(arguments,3) === true)
-})(1,2,3);
-
-(function (){    
-    console.log(area.validArgs(arguments,3) === false)
-})(1,"10",3);
-
-(function (){    
-    console.log(area.validArgs(arguments,2) === false)
-})(1, 2 ,3);
-
 //test area of circle
 console.log(area.areaOfCircle(2));
 console.log(area.areaOfCircle(0));
