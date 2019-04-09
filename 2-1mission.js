63631da54541d01b576ed8a280d7cde6e69b49dd
Minjae Lee <okssud1@naver.com>
2019-04-09 12:07:31 +0900

    2-1미션 개선 (#64)
    rest parameter 사용해서 array 생성 단순화 및 인자 갯수 확인 하위함수 생성

--- 2-1mission.js
+++ 2-1mission.js
@@ -5,53 +5,93 @@ function typeNumber(a){
     }
 }
 
-function areaOfCircle(r) {
+function checkParameter(name,para){
+    switch(name){
+        case 'Circle':
+            if(para.length !== 1){
+                throw "인자 갯수가 맞지않습니다.";
+            }else{
+                break;
+            }
+        
+        case 'Rectangular':
+            if(para.length !== 2){
+                throw "인자 갯수가 맞지않습니다.";
+            }else{
+                break;
+            }
+        
+        case 'Trapezoid':
+            if(para.length !== 3){
+                throw "인자 갯수가 맞지않습니다.";
+            }else{
+                break;
+            }
+        
+        case 'Cylinder':
+            if(para.length !== 2){
+                throw "인자 갯수가 맞지않습니다.";
+            }else{
+                break;
+            }
+    }
+
+};
+
+function areaOfCircle(...item) {
     let area = 0;
-    if(arguments.length !== 1){
-        throw "인자 갯수가 맞지않습니다.";
-    }
-    for(let i in arguments){
-        typeNumber(arguments[i]);
-    }
-    area = Math.PI*r*r;
+    let arr = item;
+    //arr = {r}
+    checkParameter('Circle',arr);
+
+    arr.forEach(function(item){
+        typeNumber(item);
+    });
+
+    area = Math.PI*item[0]*item[0];
     return area.toFixed(2);
 }
 
-function areaOfRectangular(a,b){
+function areaOfRectangular(...item){
     let area = 0;
-    if(arguments.length !== 2){
-        throw "인자 갯수가 맞지않습니다.";
-    }
-    for(let i in arguments){
-        typeNumber(arguments[i]);
-    }
-    area = a*b;
+    let arr = item;
+    //arr = {a,b}
+    checkParameter('Rectangular',arr);
+
+    arr.forEach(function(item){
+        typeNumber(item);
+    });
+    area = item[0]*item[1];
     return area;
 }
 
-function areaOfTrapezoid(a,b,h){
+function areaOfTrapezoid(...item){
     let area = 0;
-    if(arguments.length !== 2){
-        throw "인자 갯수가 맞지않습니다.";
-    }
-    for(let i in arguments){
-        typeNumber(arguments[i]);
-    }
-    area = (a+b)*h/2;
+    let arr = item;
+    //arr = {a,b,h}
+    checkParameter('Trapezoid',arr);
+
+    arr.forEach(function(item){
+        typeNumber(item);
+    });
+    area = (item[0]+item[1])*item[2]/2;
 
     return area;
 }
 
-function areaOfCylinder(r,h){
+function areaOfCylinder(...item){
     let area = 0;
-    if(arguments.length !== 2){
-        throw "인자 갯수가 맞지않습니다.";
-    }
-    for(let i in arguments){
-        typeNumber(arguments[i]);
-    }
-    let AC = areaOfCircle(r);
-    area = 2*AC+2*Math.PI*r*h;
+    let arr = item;
+    //arr = {r,h}
+
+    checkParameter('Cylinder',arr);
+
+    arr.forEach(function(item){
+        typeNumber(item);
+    });
+
+    let AC = areaOfCircle(item[0]);
+    area = 2*AC+2*Math.PI*item[0]*item[1];
     
     return area.toFixed(2);
 }
