bfe93e17f10b35fab268cba033301d441ca88661
nailerHeum <kingman330@gmail.com>
2019-04-09 17:51:21 +0900

    Complete step2-2 (#69)
    * Complete step2-2
    
    * Implement requested fixes
    
    1. var를 const와 let으로 대체
    2. 불필요한 true return 삭제
    3. saveFunctoinCallName function 만들고 사용
    4. template literal 사용, 문자열 연산 넣어둠
    5. reduce method 사용
    
    * Implement recursive function
    
    원의 넓이들을 출력하지 않고 합을 출력합니다.

--- polygon.js
+++ polygon.js
@@ -1,39 +1,144 @@
 const {PI} = Math;
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+let polygonCalls = []
 
-const checkNumber = (value) => {
-  return (typeof(value) === "number" && !isNaN(value)) ? true : false;
-}
-  
-module.exports.circle = (r) => {
-  if(!checkNumber(r)){
-    return "Input error"
-  }
-  
-  return r*r*PI;
-}
-  
-module.exports.square = (height, width) => {
-  if(!checkNumber(height) || (!checkNumber(width))){
-    return "Type error"
-  }
-
-  return height*width;
-},
-  
-module.exports.trapezoid = (height, underWidth, upperWidth) => {
-if(!checkNumber(height) || !checkNumber(underWidth) || !checkNumber(upperWidth)){
-    return "Type error"
-  }
-  return height*(underWidth + upperWidth)/2;
-},
-
-module.exports.cylinder = (r, height) => {
-  if(!checkNumber(r) || !checkNumber(height)){
-    return "Type error"
-  }
-
-  return r*r*height*PI;
+const input = (query) => {
+  return new Promise((resolve, reject) => {
+    rl.question(query, (answer) => {
+      answer *=1
+      resolve(answer)
+    })
+  })
 }
 
+const checkNumbers = (property) => {
+  function checkNum(value) {  
+    return typeof(value) === "number" && !isNaN(value)
+  }
+  return property.every(checkNum)
+}
 
+const saveFunctionCallName = (funcName) => {
+  polygonCalls.push(funcName)
+}
+
+module.exports.getCircle = async (radius) => {
+  let prop = [radius]
+  if(!checkNumbers(prop)){
+    radius = await input('[ circle radius ] >> ')
+  }
+  
+  prop = [radius]
+  if(!checkNumbers(prop)){
+    throw new Error('Input Error!')
+  }
+
+  const circle = radius*radius*PI;
+  
+  saveFunctionCallName('circle')
+  return circle
+}
+  
+module.exports.getSquare = async (height, width) => {
+  let props = [height, width]
+  if(!checkNumbers(props)){
+    height = await input('[ square height ] >> ')
+    width = await input('[ square width ] >> ')
+  }
+
+  props = [height, width]
+  if(!checkNumbers(props)){
+    throw new Error('Type Error!')
+  }
+  const square = height*width;
+  console.log('result square => ', square)
+  saveFunctionCallName('square')
+  return
+}
+  
+module.exports.getTrapezoid = async (height, underWidth, upperWidth) => {
+  let props = [height, underWidth, upperWidth]
+  if(!checkNumbers(props)){
+    height = await input('[ trapezoid height ] >> ')
+    underWidth = await input('[ trapezoid underWidth ] >> ')
+    upperWidth = await input('[ trapezoid upperWidth ] >> ')
+  }
+
+  props = [height, underWidth, upperWidth]
+  if(!checkNumbers(props)){
+    throw new Error('Type Error!')
+  }
+  const trapezoid = height*(underWidth + upperWidth)/2
+  console.log('result trapezoid => ', trapezoid)
+  saveFunctionCallName('trapezoid')
+  return
+}
+
+module.exports.getCylinder = async (radius, height) => {
+  let props = [radius, height]
+  
+  if(!checkNumbers(props)){
+    radius = await input('[ cylinder radius ] >> ')
+    height = await input('[ cylinder height ] >> ')
+  }
+
+  props = [radius, height]
+  if(!checkNumbers(props)){
+    throw new Error('Type Error!')
+  }
+
+  let cylinder = radius*radius*height*PI
+  console.log('result cylinder => ', cylinder)
+  saveFunctionCallName('cylinder')
+  return
+}
+
+module.exports.getArea = async (type, a, b, c) => {
+  let that = this
+  let sum = 0
+  const recurCircle = async (a, b) =>  {
+    if(a > b){
+      return
+    }
+    sum = sum + await that.getCircle(a)
+    await recurCircle(a+1, b)
+  }
+
+  switch (type) {
+    case 'circle' : 
+      if (checkNumbers([b])) {
+        await recurCircle(a, b)
+        console.log(`Sum of circles = ${sum} `)
+      } else {
+        await this.getCircle(a)
+      }
+      break;
+    case 'square' : 
+      await this.getSquare(a, b)
+      break;
+    case 'trapezoid' : 
+      await this.getTrapezoid(a, b, c)
+      break;
+    case 'cylinder' : 
+      await this.getCylinder(a, b)
+      break;
+    default : 
+      throw new Error('TYPE ERROR!!!')
+  }
+}
+
+module.exports.printExecutionSequence = () => {
+  let result = `수행 순서 출력 >> ${polygonCalls.reduce((acc, call) => acc + ', ' + call)}`
+  console.log(result)
+  return
+}
+
+module.exports.shutdownRl = async () => {
+  await rl.close()
+  return
+}
