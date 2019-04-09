8bcd5dee512d96beb96aa38d8e4d4f9890bf81fd
PAPICO <ocipap0531@gmail.com>
2019-04-09 20:09:03 +0900

    PAPICO & gren
    - printExecutionSequence 함수 추가
    - logExecution 함수 추가
    - 문자열 상수화

--- step2-2.js
+++ step2-2.js
@@ -1,57 +1,76 @@
-const {PI, pow} = Math
+const { PI, pow } = Math;
+const logArr = [],
+    logRes = [];
 
-const getCircleArea = (r) => {
-    let area = 0
-    area = pow(r, 2) * PI
-    return area
-}
+const CIRCLE_NAME = "circle",
+    RECT_NAME = "rect",
+    TRAPE_NAME = "trapezoid";
 
-const getTotalCircleArea = (n, res = 0) => {
-    if(0 === n) return res
-    return getTotalCircleArea(n - 1, res += getCircleArea(n))
-}
+const printExecutionSequence = _ => {
+    let log = "";
+    log = logArr.map((el, i) => `${logArr[i]} ${logRes[i]}`).reduce((a, b) => `${a}, ${b}`, log);
+    console.log(log);
+};
 
+const logExecution = (polygonName, polygonArea) => {
+    logArr.push(polygonName);
+    logRes.push(polygonArea);
+};
+
+const getCircleArea = r => {
+    let area = 0;
+    area = pow(r, 2) * PI;
+    logExecution(CIRCLE_NAME, area);
+    return area;
+};
+
+const getTotalCircleArea = (n, area = 0) => {
+    if (0 === n) {
+        logExecution(CIRCLE_NAME, area);
+        return area;
+    }
+    return getTotalCircleArea(n - 1, (area += pow(n, 2) * PI));
+};
 
 const getRectArea = (w, h) => {
-    let area = 0
-    area = w * h
-    return area
-}
+    let area = 0;
+    area = w * h;
+    logExecution(RECT_NAME, area);
+    return area;
+};
 
 const getTrapezoidArea = (uw, dw, h) => {
-    let area = 0
-    area = (uw + dw) * h / 2
-    return area
-}
-
+    let area = 0;
+    area = ((uw + dw) * h) / 2;
+    logExecution(TRAPE_NAME, area);
+    return area;
+};
 
 const getArea = (polygonName, ...params) => {
-    let area = 0
+    let area = 0;
     switch (polygonName) {
-        case "circle":
-            if(params.length === 2) {
-                area = getTotalCircleArea(params[1])
-            } else {
-                area = getCircleArea(params[0])
-            }
-            break
-        case "rect":
-            area = getRectArea(...params)
-            break
-        case "trapezoid":
-            area = getTrapezoidArea(...params)
-            break
-        default :
-            throw Error('Incorrect Parameters\ngetArea([polygon name], ...params)\npolygon name : circle / rect / trapezoid')
-            break
+        case CIRCLE_NAME:
+            area = (2 === params.length) ? getTotalCircleArea(params[1]) : getCircleArea(params[0])
+            break;
+        case RECT_NAME:
+            area = getRectArea(...params);
+            break;
+        case TRAPE_NAME:
+            area = getTrapezoidArea(...params);
+            break;
+        default:
+            throw Error(
+                "Incorrect Parameters\ngetArea([polygon name], ...params)\npolygon name : circle / rect / trapezoid"
+            );
+            break;
     }
 
-    return area
-}
+    return area;
+};
 
-console.log(getArea("circle", 1, 3))
-console.log(getArea("circle", 3))
-console.log(getArea("rect", 3, 4))
-console.log(getArea("trapezoid", 1, 2, 8))
-console.log(getArea("asas", 1))
-
+console.log(getArea("circle", 1, 3));
+console.log(getArea("circle", 3));
+console.log(getArea("rect", 3, 4));
+console.log(getArea("trapezoid", 1, 2, 8));
+printExecutionSequence();
+// console.log(getArea("asas", 1))
