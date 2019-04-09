7d16e24e0d9b16244b6d279297386e95f00f6ea0
Hoyoung Jung <honux77@gmail.com>
2019-04-09 15:09:47 +0900

    Update validation function using spread
    전개 연산자를 쓰라고 해서 썼는데..
    불편했다.

--- area.js
+++ area.js
@@ -3,37 +3,38 @@ area.js
 Caculate various area of shapes
 */
 
-function validArgs(args, length) {
-    if (args.length != length) {
+function validArgs(...args) {
+    [length, rests] = args; 
+    const arr = Array.from(rests);
+    if (arr.length != length) {
         return false
-    }
-    const arr = Array.from(args);
+    }    
     return arr.every(n => (typeof n === "number" && n != 0));
 }
 
 function areaOfCircle(radius) {
-    if(!validArgs(arguments, 1)) {
+    if(!validArgs(1, arguments)) {
         return -1;
     }    
     return Math.PI * radius * radius;
 }
 
 function areaOfRectangle(w, h) {
-    if(!validArgs(arguments, 2)) {
+    if(!validArgs(2, arguments)) {
         return -1;
     }    
     return w * h;
 }
 
 function areaOfTrapezoid(bottom, top, h) {
-    if(!validArgs(arguments, 3)) {
+    if(!validArgs(3, arguments)) {
         return -1;
     }    
     return (bottom * top) * h * 0.5;
 }
 
 function volumeOfCylinder(r, h) {
-    if(!validArgs(arguments, 2)) {
+    if(!validArgs(2, arguments)) {
         return -1;
     }    
     return areaOfCircle(r) * h;
