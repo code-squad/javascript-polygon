bb7372f674c1004cf9cb2191258fa0344511a4e4
Yongsu Jeong <jeongyongsu5366@gmail.com>
2019-04-09 12:36:55 +0900

    1. We have gathered a variety of similar methods in a module called figureArea.js.
    2. We have created the method that wehn occuring type error, return to
    the very beginning of the function recursiveAsynceFigureArea using the
    concept of recursion.
    
    3. We have maximized the reusability of the methods that we made as
    possible as we can :D

--- Figure/main.js
+++ Figure/main.js
@@ -1,20 +1,16 @@
-//const myModule = require('./module/figure');
+const figureArea = require('./module/figure/figureArea');
 const readline = require('readline');
-const circleArea = require('./module/figure/circleArea.js');
-const square = require('./module/figure/square.js');
-const trapezoid = require('./module/figure/trapezoid.js');
-const cylinder = require('./module/figure/cylinder.js');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-var recursiveAsyncReadLine = function() {
+var recursiveAsyncFigureArea = function() {
     rl.question('Which figure area do you want to know? 1. Circle 2. Square 3. Trapezoid 4. Cylinder:  ', function(answer) {
     switch(answer) {
         case "1": 
         rl.setPrompt("Please Input Radius Please: ");
         rl.prompt();
         rl.on('line', function(radius){
-            console.log(circleArea(Number(radius)));
-            rl.close();
+            const resultOfCircle = figureArea.circleArea(Number(radius));
+            errorHandler(resultOfCircle);
         });
         break;
 
@@ -23,8 +19,8 @@ var recursiveAsyncReadLine = function() {
         rl.prompt();
         rl.on('line', function(squareValues){
             var garosero = squareValues.split(" ");
-            console.log(square(Number(garosero[0]), Number(garosero[1])));
-            rl.close(); 
+            const resultOfSquare = figureArea.square(Number(garosero[0]), Number(garosero[1]));
+            errorHandler(resultOfSquare); 
         });
         break;
 
@@ -33,8 +29,8 @@ var recursiveAsyncReadLine = function() {
         rl.prompt();
         rl.on('line', function(trapezoidValues){
             var twoBaseHeight = trapezoidValues.split(" ");
-            console.log(trapezoid(Number(twoBaseHeight[0]), Number(twoBaseHeight[1]), Number(twoBaseHeight[2])));
-            rl.close();
+            const resultOfTrapezoid = figureArea.trapezoid(Number(twoBaseHeight[0]), Number(twoBaseHeight[1]), Number(twoBaseHeight[2]));
+            errorHandler(resultOfTrapezoid);
         });
         break;    
 
@@ -43,20 +39,33 @@ var recursiveAsyncReadLine = function() {
         rl.prompt();
         rl.on('line', function(cylinderValues){
             var radHei = cylinderValues.split(" ");
-            console.log(cylinder(radHei[0], radHei[1]));
-                rl.close();
+            const resultOfCylinder = figureArea.cylinder(Number(radHei[0]), Number(radHei[1]));
+            errorHandler(resultOfCylinder);
         });
         break;           
 
         default: 
             console.log("Pleas input only between 1 ~ 4");
-            recursiveAsyncReadLine()
+            recursiveAsyncFigureArea()        
         }
     })
 }
 
-recursiveAsyncReadLine();
+recursiveAsyncFigureArea();
 
 rl.on('close', function() {
     process.exit();
 });
+
+
+function errorHandler(result) {
+    if(isNaN(result)) {
+        recursiveAsyncFigureArea();
+    } 
+    
+    else {
+         console.log(result);
+         rl.close();
+    }
+}
+
--- Figure/module/figure/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    circleArea  : require('./circleArea.js'),
-    square      : require('./square.js'),
-    trapezoid   : require('./trapezoid.js'),
-    cylinder    : require('./cylinder.js')
-};
