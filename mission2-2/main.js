f81fb8233566a219211ffe896bc655743e6003e1
sonypark <sonypark0204@gmail.com>
2019-04-10 11:07:40 +0900

    Update step2-2 mission

--- mission2-2/main.js
+++ mission2-2/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const AreaOfPolygon = require('./module/AreaOfPolygon');
 
 const callStack = [];
@@ -35,7 +36,47 @@ if (require.main === module) {
     printExecutionSequence();
 } else {
     console.log('Required as a module')
+=======
+
+const AreaOfPolygon = require('./module/AreaOfPolygon');
+
+const callStack = [];
+
+function getTypeOfPolygon(typeOfPolygon, ...args) {
+
+    if (typeOfPolygon === 'circle') {
+        return AreaOfPolygon.getAreaOfCircle(...args)
+
+    } else if (typeOfPolygon === 'rect') {
+        return AreaOfPolygon.getAreaOfSquare(...args)
+
+    } else if (typeOfPolygon === 'trapezoid') {
+        return AreaOfPolygon.getAreaOfTrapezoid(...args)
+    }
+
 }
-getArea();
 
 
+function getArea(typeOfPolygon, ...args) {
+    callStack.push({ 'typeOfPolygon': typeOfPolygon, 'result': getTypeOfPolygon(typeOfPolygon, ...args) })
+    return getTypeOfPolygon(typeOfPolygon, ...args)
+}
+
+function printExecutionSequence() {
+    console.log(callStack)
+>>>>>>> Update step2-2 mission
+}
+
+<<<<<<< HEAD
+
+=======
+if (require.main === module) {
+    getArea('trapezoid', 2, 3, 5);
+    getArea('circle', 2)
+    getArea('circle', 2, 3)
+    getArea('circle', 2, 3, 4)
+    printExecutionSequence();
+} else {
+    console.log('Required as a module')
+}
+>>>>>>> Update step2-2 mission
