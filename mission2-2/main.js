1a19464e2820920dd5c3e04bc2d9009fa05c3ad9
sonypark <sonypark0204@gmail.com>
2019-04-10 11:07:40 +0900

    Update step2-2 mission

--- mission2-2/main.js
+++ mission2-2/main.js
@@ -1,71 +1,38 @@
-var readline = require('readline')
-const myModule = require('./module/areaPolygon');
-var callStack = [];
+
+const AreaOfPolygon = require('./module/AreaOfPolygon');
+
+const callStack = [];
+
+function getTypeOfPolygon(typeOfPolygon, ...args) {
+
+    if (typeOfPolygon === 'circle') {
+        return AreaOfPolygon.getAreaOfCircle(...args)
+
+    } else if (typeOfPolygon === 'rect') {
+        return AreaOfPolygon.getAreaOfSquare(...args)
+
+    } else if (typeOfPolygon === 'trapezoid') {
+        return AreaOfPolygon.getAreaOfTrapezoid(...args)
+    }
+
+}
+
+
+function getArea(typeOfPolygon, ...args) {
+    callStack.push({ 'typeOfPolygon': typeOfPolygon, 'result': getTypeOfPolygon(typeOfPolygon, ...args) })
+    return getTypeOfPolygon(typeOfPolygon, ...args)
+}
 
 function printExecutionSequence() {
-     return callStack;
+    console.log(callStack)
 }
 
-function checkNumber(args) {
-     for (let i = 0; i < args.length; i++) {
-          try {
-               if (!Number(args[i])) {
-                    throw new Error('숫자만 입력하세요.');
-               }
-          } catch (e) {
-               console.log(e.name + ': ' + e.message);
-               process.exit();
-          }
-     }
+if (require.main === module) {
+    getArea('trapezoid', 2, 3, 5);
+    getArea('circle', 2)
+    getArea('circle', 2, 3)
+    getArea('circle', 2, 3, 4)
+    printExecutionSequence();
+} else {
+    console.log('Required as a module')
 }
-
-function getArea() {
-
-     const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-     });
-     rl.setPrompt("넓이를 구하고자 하는 도형의 종류와 인자값을 입력하세요. ( 도형종류 : [circle,rect,trapezoid] )");
-     rl.prompt();
-     rl.on("line", (input) => {
-          var inputArray = input.split(',')
-          polygon = inputArray.shift();
-          args = inputArray;
-          args.forEach(function (element, index, array) {
-               array[index] = Number(element)
-          });
-          checkNumber(args);
-          rl.close();
-
-          if (polygon === 'circle') {
-               if (args.length === 1) {
-                    console.log(myModule.areaOfCircle(args));
-                    callStack.push("circle")
-               } else if (args.length === 2) {
-                    myModule.sumOfAreaOfCircle(args);
-                    callStack.push("circle")
-               } else {
-                    try {
-                         throw new Error('인자 갯수를 확인해주세요.')
-                    } catch (e) {
-                         console.log(e.name + ': ' + e.message);
-                         process.exit();
-                    }
-               }
-
-          } else if (polygon === 'rect') {
-               console.log(myModule.areaOfSquare(args));
-               callStack.push("rect")
-          } else if (polygon === 'trapezoid') {
-               console.log(myModule.areaOfTrapezoid(args));
-               callStack.push("trapezoid")
-          } else {
-               console.log("선택지에 없는 값입니다.")
-               process.exit();
-          }
-          console.log(printExecutionSequence());
-     });
-
-}
-getArea();
-
