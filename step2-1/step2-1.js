1e9b82840106f2edbf9ee9ddd835c9bd379d3dc0
Jake <cocahack@naver.com>
2019-04-11 15:00:34 +0900

    Koon & Jake Step2-2 (#91)
    * Revise codes
    Use spread operator and template literal
    
    * Step 2-2 Koon & Jake

--- step2-1/step2-1.js
+++ step2-1/step2-1.js
@@ -1,10 +1,12 @@
 const readline = require('readline');
 const { pow, PI } = Math;
-const helpMsg = '넓이를 구하는데 필요한 정보를 입력해주세요.\n' 
-               +'원 넓이: circle radius\n'
-               +'사각형 넓이: rectangle width height\n'
-               +'사다리꼴 넓이: trapezoid line1 line2 height\n'
-               +'원기둥 넓이: cylinder radius height\n';
+const helpMsg = 
+`넓이를 구하는데 필요한 정보를 입력해주세요.
+원 넓이: circle radius
+사각형 넓이: rectangle width height
+사다리꼴 넓이: trapezoid line1 line2 height
+원기둥 넓이: cylinder radius height
+`;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,19 +14,12 @@ const rl = readline.createInterface({
 });
 
 const shapeArea = {
-  circle: ([radius]) => pow(radius, 2) * PI,
-  rectangle: ([width, height]) => width * height,
-  trapezoid: ([line1, line2, height]) => 2 * height * (line1 + line2),
-  cylinder: function ([radius, height]) {
+  circle: (radius) => pow(radius, 2) * PI,
+  rectangle: (width, height) => width * height,
+  trapezoid: (line1, line2, height) => 2 * height * (line1 + line2),
+  cylinder: function (radius, height) {
     return 2 * this.circle([radius]) + circumference([radius]) * height;
   }
-}
-
-const parameterLength = {
-  circle: 1,
-  rectangle: 2,
-  trapezoid: 3,
-  cylinder: 2,
 }
 
 function circumference([radius]){
@@ -44,10 +39,10 @@ function checkNumber(args){
 }
 
 function checkArgsLength(argsLen, funcLen){
-  return argsLen >= funcLen;
+  return argsLen === funcLen;
 }
 
-function getArea(type, args){
+function getArea(type, ...args){
   try {
     if(shapeArea[type] === undefined){
       throw new Error('circle, rectangle, trapezoid, cylinder 이외에는 사용할 수 없습니다.');
@@ -57,23 +52,23 @@ function getArea(type, args){
       throw new Error('radius, line, height는 숫자여야 합니다.');
     }
 
-    if(!checkArgsLength(args.length, parameterLength[type])){
-      throw new Error('필요한 인자의 개수가 부족합니다');
+    if(!checkArgsLength(args.length, shapeArea[type].length)){
+      throw new Error('인자의 개수가 적거나 너무 많습니다.');
     }
 
-    return shapeArea[type](args);
+    return shapeArea[type](...args);
   } catch (error) {
     console.error(`에러: ${error.message}`);
   }
 }
 
 
-rl.question(helpMsg, (info) => {
+rl.question(helpMsg, info => {
   const args = info.split(' ');
   const shapeType = args[0];
   const rest = args.slice(1).map(e => parseInt(e));
   
-  const result = getArea(shapeType, rest);
+  const result = getArea(shapeType, ...rest);
 
   console.log(result || '');
 
