70941a7526aaf34ba47a40faf1ede8432afbee8d
mukeunzi <eunji057@gmail.com>
2019-04-11 18:11:33 +0900

    mission 2-2-1 getArea()

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 const area = require("./area");
 
-console.log(area.circle(3));
-console.log(area.square(5, 6));
-console.log(area.trapezoid(5, 3, 2));
-console.log(area.cylindrical(2, 5));
-
-// console.log(area.circle('3'));
-// console.log(area.square('아', 5));
-// console.log(area.trapezoid(5, 3, '3'));
-// console.log(area.cylindrical(2));
+console.log(area.getArea('circle', 10));
+console.log(area.getArea('circle', 1, 4));
+console.log(area.getArea('rect', 10, 15));
+console.log(area.getArea('trapezoid', 10, 15, 12));
