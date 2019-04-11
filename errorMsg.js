d0207e88ff13e963d005d2fee84d8a1f0b8370d3
eugene94 <eugenekang94@gmail.com>
2019-04-11 17:06:50 +0900

    JS step 2-2 구현

--- errorMsg.js
+++ errorMsg.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // "NOT_A_NUMBER" : "숫자가 아닙니다.",
-    // "WRONG_PARAMNUMBER" : "인자개수가 부족합니다.",
-    "INVALID" : "인자가 유효하지 않습니다."
-}
+  NOT_A_NUMBER: '숫자가 아닙니다.',
+  WRONG_PARAM_NUMBER: '인자개수가 부족합니다.'
+};
