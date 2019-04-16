69545b289782f99ca0b9cdfb57dd7ebf84944b94
묵은지 <39726860+mukeunzi@users.noreply.github.com>
2019-04-16 15:23:47 +0900

    Step2-2 (#94)
    * mission 2-2-1 getArea()
    
    * mission 2-2-2 printExecutionSequence()

--- area.js
+++ area.js
@@ -1,42 +1,76 @@
 const PIE = Math.PI;
+let areaSum = 0;
+let stackList = [];
 
-module.exports.circle = function(r = -1){
+function circle(r = -1){
     checkParameter(r);
+    stackList.push("circle");
     return Math.pow(r, 2) * PIE;
 }
 
-module.exports.square = function(a = -1, b = -1){
+function circleSum(r = -1){
+    checkParameter(r);
+
+    if(r > 0){
+        areaSum += Math.pow(r, 2) * PIE;
+        circleSum(r-1);
+    } else{
+        stackList.push("circle");
+    }
+
+    return areaSum;
+}
+
+function rect(a = -1, b = -1){
     checkParameter(a, b);
+    stackList.push("rect");
     return a * b;
 }
 
-module.exports.trapezoid = function(a = -1, b = -1, h = -1){
+function trapezoid(a = -1, b = -1, h = -1){
     checkParameter(a, b, h);
+    stackList.push("trapezoid");
     return (a + b) * h / 2;
 }
 
-module.exports.cylindrical = function(r = -1, h = -1){
-    checkParameter(r, h);
-    return (this.circle(r) * 2) + (r * 2 * PIE * h);
-}
-
-function checkParamCount(rest){
-    for (let i=0; i<rest.length; i++){
-        if(rest[i] === -1){
+function checkParamCount(param){
+    for (let i=0; i<param.length; i++){
+        if(param[i] === -1){
             throw Error("모든 인자를 입력하세요.");
         }
     }
 }
 
-function checkParamType(rest){
-    for (let i=0; i<rest.length; i++) {
-        if(typeof rest[i] !== "number") {
+function checkParamType(param){
+    for (let i=0; i<param.length; i++) {
+        if(typeof param[i] !== "number") {
             throw Error("숫자를 입력하세요.");
         }
     }
 }
 
-function checkParameter(...rest){
-    checkParamCount(rest);
-    checkParamType(rest);
+function checkParameter(...param){
+    checkParamCount(param);
+    checkParamType(param);
+}
+
+module.exports.printExecutionSequence = function(){
+    let result = stackList.join(', ');
+    return result;
+}
+
+module.exports.getArea = function (...param){
+    if (param[0] === 'circle'){
+        if(param.length >= 3){
+            return circleSum(param[2]);
+        }else{
+            return circle(param[1]);
+        }
+    } else if(param[0] === 'rect'){
+        return rect(param[1], param[2]);
+    } else if(param[0] === 'trapezoid'){
+        return trapezoid(param[1], param[2], param[3]);
+    }else{
+        throw Error("함수를 다시입력하세요.");
+    }
 }
